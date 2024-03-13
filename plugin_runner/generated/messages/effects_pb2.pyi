73f16a7a396786b54e24a6ef728f1f33137e16e5
Joe Wilson <joe.wilson@canvasmedical.com>
2024-03-13 14:50:32 -0700

    Adds to messages.

--- plugin_runner/generated/messages/effects_pb2.pyi
+++ plugin_runner/generated/messages/effects_pb2.pyi
@@ -1,13 +1,23 @@
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class EffectType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    UNKNOWN_EFFECT: _ClassVar[EffectType]
+    LOG: _ClassVar[EffectType]
+    ADD_PLAN_COMMAND: _ClassVar[EffectType]
+UNKNOWN_EFFECT: EffectType
+LOG: EffectType
+ADD_PLAN_COMMAND: EffectType
 
 class Effect(_message.Message):
     __slots__ = ("type", "payload")
     TYPE_FIELD_NUMBER: _ClassVar[int]
     PAYLOAD_FIELD_NUMBER: _ClassVar[int]
-    type: str
+    type: EffectType
     payload: str
-    def __init__(self, type: _Optional[str] = ..., payload: _Optional[str] = ...) -> None: ...
+    def __init__(self, type: _Optional[_Union[EffectType, str]] = ..., payload: _Optional[str] = ...) -> None: ...
