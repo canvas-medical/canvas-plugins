5e9703d38a6760ac79bfba72e8b35aabf24e6547
Michela Iannaccone <mbiannaccone@gmail.com>
2024-05-02 22:32:34 -0400

    simplify the cli and update readme

--- canvas_cli/utils/validators/tests.py
+++ canvas_cli/utils/validators/tests.py
@@ -1,34 +1,6 @@
-from unittest.mock import MagicMock, patch
-
 import pytest
 
-from canvas_cli.apps.auth.utils import get_api_client_credentials
-from canvas_cli.utils.context.context import CLIContext
-from canvas_cli.utils.validators import get_default_host, validate_manifest_file
-
-
-def test_get_default_host() -> None:
-    """Test get_default_host returns the context's default host if needed."""
-    mock_host = "mock_host"
-
-    assert get_default_host(mock_host) == mock_host
-
-    with patch.object(CLIContext, "default_host", new_callable=lambda: mock_host):
-        assert get_default_host(None) == mock_host
-
-
-@patch("keyring.get_password")
-def test_get_api_client_credentials(mock_get_password: MagicMock) -> None:
-    """Test get_api_client_credentials returns the given api_key or fetches from `get_password`."""
-    mock_get_password.return_value = "a_password"
-
-    mock_host = "a_host"
-    assert get_api_client_credentials(mock_host, None, None) == "a_password"
-    assert get_api_client_credentials(mock_host, None, "secret") == "a_password"
-    assert get_api_client_credentials(mock_host, "id", None) == "a_password"
-    assert (
-        get_api_client_credentials(mock_host, "id", "secret") == "client_id=id&client_secret=secret"
-    )
+from canvas_cli.utils.validators import validate_manifest_file
 
 
 @pytest.fixture
