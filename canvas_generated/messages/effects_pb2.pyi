63fc4e86c7cb92e1ad1effa2b2c005e8f19bd300
Michela Iannaccone <mbiannaccone@gmail.com>
2024-06-20 15:45:21 -0400

    fix precommit and init and generated/

--- canvas_generated/messages/effects_pb2.pyi
+++ canvas_generated/messages/effects_pb2.pyi
@@ -1,10 +1,7 @@
-from typing import ClassVar as _ClassVar
-from typing import Optional as _Optional
-from typing import Union as _Union
-
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -16,7 +13,6 @@ class EffectType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     AUTOCOMPLETE_SEARCH_RESULTS: _ClassVar[EffectType]
     ADD_BANNER_ALERT: _ClassVar[EffectType]
     REMOVE_BANNER_ALERT: _ClassVar[EffectType]
-
 UNKNOWN_EFFECT: EffectType
 LOG: EffectType
 ADD_PLAN_COMMAND: EffectType
@@ -32,9 +28,4 @@ class Effect(_message.Message):
     type: EffectType
     payload: str
     plugin_name: str
-    def __init__(
-        self,
-        type: _Optional[_Union[EffectType, str]] = ...,
-        payload: _Optional[str] = ...,
-        plugin_name: _Optional[str] = ...,
-    ) -> None: ...
+    def __init__(self, type: _Optional[_Union[EffectType, str]] = ..., payload: _Optional[str] = ..., plugin_name: _Optional[str] = ...) -> None: ...
---
+++ canvas_sdk/effects/banner_alert/__init__.py
@@ -0,0 +1,2 @@
+from canvas_sdk.effects.banner_alert.add_banner_alert import AddBannerAlert
+from canvas_sdk.effects.banner_alert.remove_banner_alert import RemoveBannerAlert
