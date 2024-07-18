f0534c60bd62e353c3567fed84f78e57d1ad78ab
Christopher Sande <christopher.sande@canvasmedical.com>
2024-07-18 12:00:44 -0500

    Improve handling of optional fields; parse birth date correctly

--- canvas_sdk/data/patient.py
+++ canvas_sdk/data/patient.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from typing import Self
 
 from canvas_sdk.data import DataModel
@@ -12,15 +12,15 @@ class Patient(DataModel):
     id: str | None = None
     first_name: str | None = None
     last_name: str | None = None
-    birth_date: datetime | None = None
+    birth_date: date | None = None
 
     @classmethod
     def get(cls, id: str) -> Self:
         """Given an ID, get the Patient from the Data Access Layer."""
         patient = dal_client.get_patient(id)
-        return cls(  # type: ignore[call-arg]
+        return cls(
             id=patient.id,
-            first_name=patient.first_name,
-            last_name=patient.last_name,
-            birth_date=patient.birth_date,
+            first_name=patient.first_name or None,
+            last_name=patient.last_name or None,
+            birth_date=date.fromisoformat(patient.birth_date) if patient.birth_date else None,
         )
