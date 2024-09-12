697e00eb2cef63f021fdfe340e012b7388e4b7e2
Michela Iannaccone <mbiannaccone@gmail.com>
2024-09-12 10:54:48 -0400

    clean up banner alert test a little bit more

--- canvas_sdk/effects/banner_alert/tests.py
+++ canvas_sdk/effects/banner_alert/tests.py
@@ -1,11 +1,11 @@
 import shutil
 from contextlib import chdir
+from datetime import datetime
 from pathlib import Path
-from typing import Any
+from typing import Any, Generator
 
 import pytest
 import requests
-from pydantic import ValidationError
 from typer.testing import CliRunner
 
 import settings
@@ -31,7 +31,8 @@ def token() -> MaskedValue:
     )
 
 
-def get_first_patient_id(token: MaskedValue) -> dict:
+@pytest.fixture(scope="session")
+def first_patient_id(token: MaskedValue) -> dict:
     headers = {
         "Authorization": f"Bearer {token.value}",
         "Content-Type": "application/json",
@@ -41,16 +42,20 @@ def get_first_patient_id(token: MaskedValue) -> dict:
     return patients["entry"][0]["resource"]["key"]
 
 
-@pytest.mark.integtest
-def test_protocol_that_adds_banner_alert(
-    token: MaskedValue,
-) -> None:
-    patient_id = get_first_patient_id(token)
+@pytest.fixture(scope="session")
+def plugin_name() -> str:
+    return f"addbanneralert{datetime.now().timestamp()}".replace(".", "")
 
+
+@pytest.fixture(autouse=True, scope="session")
+def write_and_install_protocol_and_clean_up(
+    first_patient_id: str, plugin_name: str, token: MaskedValue
+) -> Generator[Any, Any, Any]:
+    # write the protocol
     with chdir(Path("./custom-plugins")):
-        runner.invoke(app, "init", input="add_banner_alert")
+        runner.invoke(app, "init", input=plugin_name)
 
-    protocol = open("./custom-plugins/add_banner_alert/protocols/my_protocol.py", "w")
+    protocol = open(f"./custom-plugins/{plugin_name}/protocols/my_protocol.py", "w")
     protocol.write(
         f"""from canvas_sdk.effects.banner_alert import AddBannerAlert
 from canvas_sdk.events import EventType
@@ -61,8 +66,8 @@ class Protocol(BaseProtocol):
     def compute(self):
         return [
             AddBannerAlert(
-                patient_id="{patient_id}",
-                key="banners-test",
+                patient_id="{first_patient_id}",
+                key="{plugin_name}",
                 narrative="this is a test",
                 placement=[AddBannerAlert.Placement.CHART],
                 intent=AddBannerAlert.Intent.INFO,
@@ -76,10 +81,47 @@ class Protocol(BaseProtocol):
     requests.post(
         plugin_url(settings.INTEGRATION_TEST_URL),
         data={"is_enabled": True},
-        files={"package": open(_build_package(Path("./custom-plugins/add_banner_alert")), "rb")},
+        files={"package": open(_build_package(Path(f"./custom-plugins/{plugin_name}")), "rb")},
         headers={"Authorization": f"Bearer {token.value}"},
     )
 
+    yield
+
+    # clean up
+    if Path(f"./custom-plugins/{plugin_name}").exists():
+        shutil.rmtree(Path(f"./custom-plugins/{plugin_name}"))
+
+    # disable
+    requests.patch(
+        plugin_url(settings.INTEGRATION_TEST_URL, plugin_name),
+        data={"is_enabled": False},
+        headers={
+            "Authorization": f"Bearer {token.value}",
+        },
+    )
+    # delete
+    requests.delete(
+        plugin_url(settings.INTEGRATION_TEST_URL, plugin_name),
+        headers={"Authorization": f"Bearer {token.value}"},
+    )
+
+    # confirm no more banner
+    patient_banners_none = requests.get(
+        f"{settings.INTEGRATION_TEST_URL}/api/BannerAlert/?patient__key={first_patient_id}",
+        headers={
+            "Authorization": f"Bearer {token.value}",
+        },
+    ).json()
+    patient_banner = next(
+        (b for b in patient_banners_none["results"] if b["key"] == plugin_name), None
+    )
+    assert patient_banner is None
+
+
+@pytest.mark.integtest
+def test_protocol_that_adds_banner_alert(
+    token: MaskedValue, plugin_name: str, first_patient_id: str
+) -> None:
     # trigger the event
     requests.post(
         f"{settings.INTEGRATION_TEST_URL}/api/Note/",
@@ -98,47 +140,17 @@ class Protocol(BaseProtocol):
     )
 
     patient_banners = requests.get(
-        f"{settings.INTEGRATION_TEST_URL}/api/BannerAlert/?patient__key={patient_id}",
+        f"{settings.INTEGRATION_TEST_URL}/api/BannerAlert/?patient__key={first_patient_id}",
         headers={
             "Authorization": f"Bearer {token.value}",
         },
     ).json()
     assert patient_banners["count"] > 0
 
-    patient_banner = next(b for b in patient_banners["results"] if b["key"] == "banners-test")
-    assert patient_banner["pluginName"] == "add_banner_alert"
+    patient_banner = next(b for b in patient_banners["results"] if b["key"] == plugin_name)
+    assert patient_banner["pluginName"] == plugin_name
     assert patient_banner["narrative"] == "this is a test"
     assert patient_banner["placement"] == ["chart"]
     assert patient_banner["intent"] == "info"
     assert patient_banner["href"] is None
     assert patient_banner["status"] == "active"
-
-    # clean up
-    if Path(f"./custom-plugins/add_banner_alert").exists():
-        shutil.rmtree(Path(f"./custom-plugins/add_banner_alert"))
-
-    # disable
-    requests.patch(
-        plugin_url(settings.INTEGRATION_TEST_URL, "add_banner_alert"),
-        data={"is_enabled": False},
-        headers={
-            "Authorization": f"Bearer {token.value}",
-        },
-    )
-    # delete
-    requests.delete(
-        plugin_url(settings.INTEGRATION_TEST_URL, "add_banner_alert"),
-        headers={"Authorization": f"Bearer {token.value}"},
-    )
-
-    # confirm no more banner
-    patient_banners_none = requests.get(
-        f"{settings.INTEGRATION_TEST_URL}/api/BannerAlert/?patient__key={patient_id}",
-        headers={
-            "Authorization": f"Bearer {token.value}",
-        },
-    ).json()
-    patient_banner = next(
-        (b for b in patient_banners_none["results"] if b["key"] == "banners-test"), None
-    )
-    assert patient_banner is None
