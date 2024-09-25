38aed42b59e0b43d6512d87c61e41f781d32208b
Joe Wilson <joe.wilson@canvasmedical.com>
2024-09-25 14:40:49 -0700

    Changes database view name for CanvasUser.

--- canvas_sdk/v1/data/user.py
+++ canvas_sdk/v1/data/user.py
@@ -5,6 +5,6 @@ class CanvasUser(models.Model):
     class Meta:
         managed = False
         app_label = "canvas_sdk"
-        db_table = "canvas_sdk_data_api_canvas_user_001"
+        db_table = "canvas_sdk_data_api_auth_user_001"
 
     dbid = models.BigIntegerField(db_column="dbid", primary_key=True)
