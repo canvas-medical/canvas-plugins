321e881f01869d07762c823e6d4a139929e34134
Joe Wilson <joe.wilson@canvasmedical.com>
2024-09-25 14:27:57 -0700

    Removes unnecessary arguments to model fields.

--- canvas_sdk/v1/data/lab.py
+++ canvas_sdk/v1/data/lab.py
@@ -17,28 +17,24 @@ class LabReport(models.Model):
     dbid = models.BigIntegerField(primary_key=True)
     created = models.DateTimeField()
     modified = models.DateTimeField()
-    review_mode = models.CharField(max_length=2)
+    review_mode = models.CharField()
     junked = models.BooleanField()
     requires_signature = models.BooleanField()
-    assigned_date = models.DateTimeField(null=True)
-    patient = models.ForeignKey(
-        Patient, on_delete=models.DO_NOTHING, related_name="lab_reports", null=True
-    )
-    transmission_type = models.CharField(max_length=2)
+    assigned_date = models.DateTimeField()
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING, related_name="lab_reports")
+    transmission_type = models.CharField()
     for_test_only = models.BooleanField()
-    report_data_payload_cache = models.JSONField(blank=True, null=True)
-    external_id = models.CharField(max_length=40, blank=True)
+    report_data_payload_cache = models.JSONField()
+    external_id = models.CharField()
     version = models.IntegerField()
-    requisition_number = models.CharField(max_length=40, blank=True)
-    review = models.ForeignKey(
-        "LabReview", related_name="reports", null=True, on_delete=models.DO_NOTHING
-    )
-    original_date = models.DateTimeField(null=True)
-    date_performed = models.DateTimeField(null=True)
-    custom_document_name = models.CharField(blank=True)
-    originator = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING, null=True)
-    committer = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING, null=True)
-    entered_in_error = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING, null=True)
+    requisition_number = models.CharField()
+    review = models.ForeignKey("LabReview", related_name="reports", on_delete=models.DO_NOTHING)
+    original_date = models.DateTimeField()
+    date_performed = models.DateTimeField()
+    custom_document_name = models.CharField()
+    originator = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING)
+    committer = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING)
+    entered_in_error = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING)
     deleted = models.BooleanField()
 
 
@@ -54,15 +50,15 @@ class LabReview(models.Model):
     dbid = models.BigIntegerField(primary_key=True)
     created = models.DateTimeField()
     modified = models.DateTimeField()
-    originator = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING, null=True)
+    originator = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING)
     deleted = models.BooleanField()
-    committer = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING, null=True)
-    entered_in_error = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING, null=True)
-    internal_comment = models.TextField(blank=True)
-    message_to_patient = models.CharField(max_length=2048, blank=True)
-    status = models.CharField(max_length=50, blank=True)
+    committer = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING)
+    entered_in_error = models.ForeignKey(CanvasUser, on_delete=models.DO_NOTHING)
+    internal_comment = models.TextField()
+    message_to_patient = models.CharField()
+    status = models.CharField()
     patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
-    patient_communication_method = models.CharField(max_length=30)
+    patient_communication_method = models.CharField()
 
 
 class LabValue(models.Model):
@@ -75,17 +71,15 @@ class LabValue(models.Model):
     dbid = models.BigIntegerField(primary_key=True)
     created = models.DateTimeField()
     modified = models.DateTimeField()
-    report = models.ForeignKey(
-        "LabReport", related_name="values", null=True, on_delete=models.DO_NOTHING
-    )
-    value = models.TextField(blank=True)
-    units = models.CharField(max_length=30, blank=True)
-    abnormal_flag = models.CharField(max_length=128, blank=True)
-    reference_range = models.CharField(max_length=128, blank=True)
-    low_threshold = models.CharField(max_length=30, blank=True)
-    high_threshold = models.CharField(max_length=30, blank=True)
-    comment = models.TextField(max_length=2000, blank=True)
-    observation_status = models.CharField(blank=True, max_length=24)
+    report = models.ForeignKey("LabReport", related_name="values", on_delete=models.DO_NOTHING)
+    value = models.TextField()
+    units = models.CharField()
+    abnormal_flag = models.CharField()
+    reference_range = models.CharField()
+    low_threshold = models.CharField()
+    high_threshold = models.CharField()
+    comment = models.TextField()
+    observation_status = models.CharField()
 
 
 class LabValueCoding(models.Model):
@@ -97,9 +91,7 @@ class LabValueCoding(models.Model):
     dbid = models.BigIntegerField(primary_key=True)
     created = models.DateTimeField()
     modified = models.DateTimeField()
-    value = models.ForeignKey(
-        LabValue, on_delete=models.DO_NOTHING, related_name="codings", null=True
-    )
-    code = models.CharField(max_length=128, blank=True)
-    name = models.CharField(max_length=256, blank=True)
+    value = models.ForeignKey(LabValue, on_delete=models.DO_NOTHING, related_name="codings")
+    code = models.CharField()
+    name = models.CharField()
     # there is no system in the database; these are all assumed to be LOINC codes
