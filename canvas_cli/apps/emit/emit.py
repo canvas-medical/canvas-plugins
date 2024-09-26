1f1b5594e599fc41bc25853e7c0c6f0e9728dba5
Andrew Duane <andrew.duane@canvasmedical.com>
2024-09-25 19:46:18 -0700

    Better error messages in the CLI emit command, more accessible event fixtures.

--- canvas_cli/apps/emit/emit.py
+++ canvas_cli/apps/emit/emit.py
@@ -22,8 +22,24 @@ def emit(
 
     Valid fixture files are newline-delimited JSON, with each containing the keys `EventType`, `target`, and `context`. Some fixture files are included in the canvas-plugins repo.
     """
+    # If an event fixture file exists at the specified path, use it.
+    # Otherwise, see if it represents an event that we have a Canvas-provided
+    # fixture for and use that.
+    event_fixture_path = Path(event_fixture)
+
+    if not event_fixture_path.exists():
+        candidate_built_in_fixture_path = (
+            Path(__file__).resolve().parent / "event_fixtures" / f"{event_fixture}.ndjson"
+        )
+        if candidate_built_in_fixture_path.exists():
+            event_fixture_path = candidate_built_in_fixture_path
+        else:
+            print(f"ERROR: No file found at location {event_fixture}.")
+            print(f"ERROR: No built-in fixture file found named {event_fixture}.ndjson.")
+            return
+
     # Grab a random event from the fixture file ndjson
-    lines = Path(event_fixture).read_text().splitlines()
+    lines = event_fixture_path.read_text().splitlines()
     myline = random.choice(lines)
     event_data = json.loads(myline)
     event = PluginRunnerEvent(
@@ -35,6 +51,17 @@ def emit(
         stub = PluginRunnerStub(channel)
         responses = stub.HandleEvent(event)
 
-        for response in responses:
-            for effect in response.effects:
-                print(effect)
+        at_least_one_effect = False
+        try:
+            for response in responses:
+                for effect in response.effects:
+                    at_least_one_effect = True
+                    print(effect)
+
+            if not at_least_one_effect:
+                print("SUCCESS: No effects returned.")
+        except grpc.RpcError as e:
+            if e.code() == grpc.StatusCode.UNAVAILABLE:
+                print(
+                    f"ERROR: Couldn't make a connection to a plugin runner process at localhost:{plugin_runner_port}. Is it running?"
+                )
