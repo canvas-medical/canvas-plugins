f861618981e1ef43f05fe318c78937577e577cc0
Andrew Duane <andrew.duane@canvasmedical.com>
2024-09-25 19:07:25 -0700

    Warn, do not fail if PLUGIN_RUNNER_SIGNING_KEY is not set.

--- plugin_runner/authentication.py
+++ plugin_runner/authentication.py
@@ -4,14 +4,20 @@ from typing import cast
 import arrow
 from jwt import encode
 
+from logger import log
+
 ONE_DAY_IN_MINUTES = 60 * 24
+
+INSECURE_DEFAULT_SIGNING_KEY = "INSECURE_KEY"
 
 
 def token_for_plugin(
     plugin_name: str,
     audience: str,
     issuer: str = "plugin-runner",
-    jwt_signing_key: str = cast(str, os.getenv("PLUGIN_RUNNER_SIGNING_KEY")),
+    jwt_signing_key: str = cast(
+        str, os.getenv("PLUGIN_RUNNER_SIGNING_KEY", INSECURE_DEFAULT_SIGNING_KEY)
+    ),
     expiration_minutes: int = ONE_DAY_IN_MINUTES,
     extra_kwargs: dict | None = None,
 ) -> str:
@@ -20,6 +26,11 @@ def token_for_plugin(
     """
     if not extra_kwargs:
         extra_kwargs = {}
+
+    if jwt_signing_key == INSECURE_DEFAULT_SIGNING_KEY:
+        log.warning(
+            "Using an insecure JWT signing key for GraphQL access. Set the PLUGIN_RUNNER_SIGNING_KEY environment variable to avoid this message."
+        )
 
     token = encode(
         {
