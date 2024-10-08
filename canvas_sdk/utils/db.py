6eb142413dd1ac0008c5e9d85094112be5e32075
Joe Wilson <joe.wilson@canvasmedical.com>
2024-10-08 12:05:49 -0700

    Simplifies url parsing.

--- canvas_sdk/utils/db.py
+++ canvas_sdk/utils/db.py
@@ -1,19 +1,16 @@
+from typing import Any
 from urllib import parse
 
 
-def parse_database_url(url: str) -> dict[str, str]:
+def parse_database_url(url: str) -> dict[str, Any]:
     """Parses a database URL and returns it in the format expected in Django settings."""
     parsed_url = parse.urlparse(url)
-    netloc_split = parsed_url.netloc.split(":")
-    db_username = netloc_split[0]
-    db_port = netloc_split[2]
-    db_password, db_host = netloc_split[1].split("@")
     db_name = parsed_url.path[1:]
     return {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": db_name,
-        "USER": db_username,
-        "PASSWORD": db_password,
-        "HOST": db_host,
-        "PORT": db_port,
+        "USER": parsed_url.username,
+        "PASSWORD": parsed_url.password,
+        "HOST": parsed_url.hostname,
+        "PORT": parsed_url.port,
     }
