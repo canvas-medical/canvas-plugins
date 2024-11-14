f6b96bfa993df812f9a1d59918b6847640f85d80
Joe Wilson <joe.wilson@canvasmedical.com>
2024-11-13 16:41:15 -0800

    Removes duplication after rebase.

--- canvas_sdk/v1/data/patient.py
+++ canvas_sdk/v1/data/patient.py
@@ -57,26 +57,7 @@ class Patient(models.Model):
 
     def age_at(self, time: arrow.Arrow) -> float:
         """Given a datetime, returns what the patient's age would be at that datetime."""
-        age = 0
-        birth_date = arrow.get(self.birth_date)
-        if birth_date.date() < time.date():
-            age = time.datetime.year - birth_date.datetime.year
-            if time.datetime.month < birth_date.datetime.month or (
-                time.datetime.month == birth_date.datetime.month
-                and time.datetime.day < birth_date.datetime.day
-            ):
-                age -= 1
-
-            current_year = birth_date.shift(years=age)
-            next_year = birth_date.shift(years=age + 1)
-            age += (time.date() - current_year.date()) / (next_year.date() - current_year.date())
-        return age
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
-    def age_at(self, time: arrow.Arrow) -> float:
-        """Given a datetime, returns what the patient's age would be at that datetime."""
-        age = 0
+        age = float(0)
         birth_date = arrow.get(self.birth_date)
         if birth_date.date() < time.date():
             age = time.datetime.year - birth_date.datetime.year
