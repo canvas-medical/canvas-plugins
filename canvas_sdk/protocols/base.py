f910345f7763add4f60f3b671d97c8596b126d45
Joe Wilson <joe.wilson@canvasmedical.com>
2024-11-25 11:07:08 -0800

    Removes data directory and gql from project.

--- canvas_sdk/protocols/base.py
+++ canvas_sdk/protocols/base.py
@@ -1,8 +1,5 @@
 from typing import Any
 
-import arrow
-
-from canvas_sdk.data.client import GQL_CLIENT
 from canvas_sdk.handlers.base import BaseHandler
 
 
@@ -11,13 +8,4 @@ class BaseProtocol(BaseHandler):
     The class that protocols inherit from.
     """
 
-    def _beta_run_gql_query(self, query: str, variables: dict | None = None) -> dict[str, Any]:
-        return GQL_CLIENT.query(
-            query,
-            variables=variables,
-            extra_args={
-                "headers": {
-                    "Authorization": f'Bearer {self.secrets["graphql_jwt"]}',
-                },
-            },
-        )
+    pass
