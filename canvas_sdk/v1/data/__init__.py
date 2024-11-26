0fc55901111d770bad6de5d1f6bdc160c33d7b5c
Joe Wilson <joe.wilson@canvasmedical.com>
2024-11-25 16:41:19 -0800

    feat: add models for task, task labels, task comments and staff (#202)

--- canvas_sdk/v1/data/__init__.py
+++ canvas_sdk/v1/data/__init__.py
@@ -1,3 +1,5 @@
 from .condition import Condition, ConditionCoding
 from .medication import Medication, MedicationCoding
 from .patient import Patient
+from .staff import Staff
+from .task import Task, TaskComment, TaskLabel
