ea80d804a36b0aab5249cf42670b2d773236b1aa
Joe Wilson <joe.wilson@canvasmedical.com>
2024-11-25 17:24:13 -0800

    feat: moves task effects (#208)

--- canvas_sdk/data/patient.py
+++
@@ -1,8 +0,0 @@
-from canvas_sdk.data import DataModel
-
-
-class Patient(DataModel):
-    """Data model for a patient."""
-
-    id: str | None = None
-    # TODO - populate more attributes
--- canvas_sdk/data/staff.py
+++
@@ -1,8 +0,0 @@
-from canvas_sdk.data import DataModel
-
-
-class Staff(DataModel):
-    """Data model for a staff member."""
-
-    id: str | None = None
-    # TODO - populate with first name, last name, additional attributes, etc.
