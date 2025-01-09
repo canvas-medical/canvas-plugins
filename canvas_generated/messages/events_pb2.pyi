cd50c6e951e72b2776bdb98320e23ae232ea0380
Miguel Quintas <miguelquintas@Mac.lan>
2025-01-09 11:18:00 +0000

    fix: rename events

--- canvas_generated/messages/events_pb2.pyi
+++ canvas_generated/messages/events_pb2.pyi
@@ -659,8 +659,8 @@ class EventType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     PLUGIN_UPDATED: _ClassVar[EventType]
     APPLICATION__ON_OPEN: _ClassVar[EventType]
     PATIENT_PORTAL__GET_INTAKE_FORMS: _ClassVar[EventType]
-    PATIENT_PORTAL__CANCEL_APPOINTMENT: _ClassVar[EventType]
-    PATIENT_PORTAL__RESCHEDULE_APPOINTMENT: _ClassVar[EventType]
+    PATIENT_PORTAL__APPOINTMENT_CANCELED: _ClassVar[EventType]
+    PATIENT_PORTAL__APPOINTMENT_RESCHEDULED: _ClassVar[EventType]
 UNKNOWN: EventType
 ALLERGY_INTOLERANCE_CREATED: EventType
 ALLERGY_INTOLERANCE_UPDATED: EventType
@@ -1311,8 +1311,8 @@ PLUGIN_CREATED: EventType
 PLUGIN_UPDATED: EventType
 APPLICATION__ON_OPEN: EventType
 PATIENT_PORTAL__GET_INTAKE_FORMS: EventType
-PATIENT_PORTAL__CANCEL_APPOINTMENT: EventType
-PATIENT_PORTAL__RESCHEDULE_APPOINTMENT: EventType
+PATIENT_PORTAL__APPOINTMENT_CANCELED: EventType
+PATIENT_PORTAL__APPOINTMENT_RESCHEDULED: EventType
 
 class Event(_message.Message):
     __slots__ = ("type", "target", "context", "target_type")
