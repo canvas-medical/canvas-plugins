99b693a2d888409ddf46391135d3ecfeb9391d14
Christopher Sande <christopher.sande@canvasmedical.com>
2025-01-09 16:09:22 -0600

    Added Interview as a model that can be obtained with get_model

--- canvas_sdk/models/__init__.py
+++ canvas_sdk/models/__init__.py
@@ -5,3 +5,4 @@ from canvas_sdk.v1.data.condition import Condition
 from canvas_sdk.v1.data.note import Note
 from canvas_sdk.v1.data.patient import Patient
 from canvas_sdk.v1.data.user import CanvasUser
+from canvas_sdk.v1.data.questionnaire import Interview
