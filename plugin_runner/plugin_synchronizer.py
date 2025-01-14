457077fa77fc2007d0ffbd2915fbb4eb8d2a1bb5
David Jantzen <mr.djantzen@gmail.com>
2025-01-14 11:40:06 -0800

    send sighup after installing when synchronizer boots, remove load command when ReloadPlugins is received since this occurs on plugin_runner start

--- plugin_runner/plugin_synchronizer.py
+++ plugin_runner/plugin_synchronizer.py
@@ -48,6 +48,8 @@ def main() -> None:
     try:
         print("plugin-synchronizer: installing plugins after web container start")
         install_plugins()
+        print("plugin-synchronizer: sending SIGHUP to plugin-runner")
+        check_output(["circusctl", "signal", "plugin-runner", "1"], cwd="/app", stderr=STDOUT)
     except CalledProcessError as e:
         print("plugin-synchronizer: `install_plugins` failed:", e)
 
