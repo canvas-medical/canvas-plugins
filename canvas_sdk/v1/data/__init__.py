a3d363d45b284519c0e87de3ac23ae72571b1371
Joe Wilson <joe.wilson@canvasmedical.com>
2025-01-14 15:24:40 -0800

    Adds PatientSetting to the __init__ file.

--- canvas_sdk/v1/data/__init__.py
+++ canvas_sdk/v1/data/__init__.py
@@ -4,7 +4,13 @@ from .condition import Condition, ConditionCoding
 from .coverage import Coverage, Transactor, TransactorAddress, TransactorPhone
 from .medication import Medication, MedicationCoding
 from .organization import Organization
-from .patient import Patient, PatientAddress, PatientContactPoint, PatientExternalIdentifier
+from .patient import (
+    Patient,
+    PatientAddress,
+    PatientContactPoint,
+    PatientExternalIdentifier,
+    PatientSetting,
+)
 from .practicelocation import PracticeLocation
 from .staff import Staff
 from .task import Task, TaskComment, TaskLabel
@@ -23,6 +29,7 @@ __all__ = (
     "PatientAddress",
     "PatientContactPoint",
     "PatientExternalIdentifier",
+    "PatientSetting",
     "PracticeLocation",
     "Staff",
     "Task",
