4424ea534adf9b34547bee2abbd454f0b9dc54d0
Joe Wilson <joe.wilson@canvasmedical.com>
2025-01-13 16:49:31 -0800

    Additional WIP additions.

--- canvas_sdk/v1/data/__init__.py
+++ canvas_sdk/v1/data/__init__.py
@@ -1,14 +1,17 @@
 from .billing import BillingLineItem
+from .care_team import CareTeamMembership, CareTeamRole
 from .condition import Condition, ConditionCoding
 from .medication import Medication, MedicationCoding
 from .organization import Organization
-from .patient import Patient, PatientAddress, PatientContactPoint
+from .patient import Patient, PatientAddress, PatientContactPoint, PatientExternalIdentifier
 from .practicelocation import PracticeLocation
 from .staff import Staff
 from .task import Task, TaskComment, TaskLabel
 
 __all__ = (
     "BillingLineItem",
+    "CareTeamMembership",
+    "CareTeamRole",
     "Condition",
     "ConditionCoding",
     "Medication",
@@ -17,6 +20,7 @@ __all__ = (
     "Patient",
     "PatientAddress",
     "PatientContactPoint",
+    "PatientExternalIdentifier",
     "PracticeLocation",
     "Staff",
     "Task",
