5a1b5bb66705d0ba0918def2578cd47ab1dcf60f
Christopher Sande <christopher.sande@canvasmedical.com>
2025-01-15 11:40:10 -0500

    feat: added new CreateQuestionnaireResult effect type (#250)
    Signed-off-by: Christopher Sande <christopher.sande@canvasmedical.com>
    Co-authored-by: Reba Magier <reba.magier@canvasmedical.com>

---
+++ canvas_sdk/effects/questionnaire_result.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+from canvas_sdk.effects.base import EffectType, _BaseEffect
+
+
+class CreateQuestionnaireResult(_BaseEffect):
+    """CreateQuestionnaireResult effect."""
+
+    class Meta:
+        effect_type = EffectType.CREATE_QUESTIONNAIRE_RESULT
+
+    interview_id: str | None = None
+    score: float | None = None
+    abnormal: bool | None = None
+    narrative: str | None = None
+    code_system: str | None = None
+    code: str | None = None
+
+    @property
+    def values(self) -> dict[str, Any]:
+        """Make the payload."""
+        return {
+            "interview_id": self.interview_id,
+            "score": self.score,
+            "abnormal": self.abnormal or False,
+            "narrative": self.narrative or "",
+            "code_system": self.code_system,
+            "code": self.code,
+        }
