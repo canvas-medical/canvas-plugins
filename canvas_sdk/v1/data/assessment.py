40c396e59e43722a9220f52f1affa25a71cf671d
Joe Wilson <joe.wilson@canvasmedical.com>
2025-01-22 14:38:34 -0800

    Adds v1 references and removes app_label from the Assessment model.

--- canvas_sdk/v1/data/assessment.py
+++ canvas_sdk/v1/data/assessment.py
@@ -14,27 +14,26 @@ class Assessment(models.Model):
 
     class Meta:
         managed = False
-        app_label = "canvas_sdk"
         db_table = "canvas_sdk_data_api_assessment_001"
 
     id = models.UUIDField()
     dbid = models.BigIntegerField(primary_key=True)
     created = models.DateTimeField()
     modified = models.DateTimeField()
-    originator = models.ForeignKey("CanvasUser", on_delete=models.DO_NOTHING)
-    committer = models.ForeignKey("CanvasUser", on_delete=models.DO_NOTHING, null=True)
+    originator = models.ForeignKey("v1.CanvasUser", on_delete=models.DO_NOTHING)
+    committer = models.ForeignKey("v1.CanvasUser", on_delete=models.DO_NOTHING, null=True)
     deleted = models.BooleanField()
-    entered_in_error = models.ForeignKey("CanvasUser", on_delete=models.DO_NOTHING, null=True)
+    entered_in_error = models.ForeignKey("v1.CanvasUser", on_delete=models.DO_NOTHING, null=True)
     patient = models.ForeignKey(
-        "Patient",
+        "v1.Patient",
         on_delete=models.DO_NOTHING,
         related_name="assessments",
     )
-    note = models.ForeignKey("Note", on_delete=models.DO_NOTHING, related_name="assessments")
+    note = models.ForeignKey("v1.Note", on_delete=models.DO_NOTHING, related_name="assessments")
     condition = models.ForeignKey(
-        "Condition", on_delete=models.CASCADE, related_name="assessments", null=True
+        "v1.Condition", on_delete=models.CASCADE, related_name="assessments", null=True
     )
-    interview = models.ForeignKey("Interview", on_delete=models.DO_NOTHING, null=True)
+    interview = models.ForeignKey("v1.Interview", on_delete=models.DO_NOTHING, null=True)
     status = models.CharField(choices=AssessmentStatus.choices)
     narrative = models.CharField()
     background = models.CharField()
