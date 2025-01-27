fd3e9899ea13c74bc1ae4fc8570ba357b9326760
Christopher Sande <christopher.sande@canvasmedical.com>
2025-01-27 15:59:07 -0600

    Added a few missing __init__.py files

---
+++ canvas_sdk/effects/surescripts/__init__.py
@@ -0,0 +1,11 @@
+from .surescripts_messages import (
+    SendSurescriptsBenefitsRequestEffect,
+    SendSurescriptsEligibilityRequestEffect,
+    SendSurescriptsMedicationHistoryRequestEffect,
+)
+
+__all__ = [
+    "SendSurescriptsBenefitsRequestEffect",
+    "SendSurescriptsEligibilityRequestEffect",
+    "SendSurescriptsMedicationHistoryRequestEffect",
+]
