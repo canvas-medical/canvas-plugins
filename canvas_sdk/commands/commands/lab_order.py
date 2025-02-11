ebc586c91e22782bbd1a3497be5a9110caa00e87
Nuno Silva <nuno.silva@canvasmedical.com>
2025-02-11 17:27:56 +0000

    add partner lab models and update lab order command to validate results

--- canvas_sdk/commands/commands/lab_order.py
+++ canvas_sdk/commands/commands/lab_order.py
@@ -1,4 +1,10 @@
+from typing import Literal
+
+from django.db.models.query_utils import Q
+from pydantic_core import InitErrorDetails
+
 from canvas_sdk.commands.base import _BaseCommand as BaseCommand
+from canvas_sdk.v1.data.lab import LabPartner, LabPartnerTest
 
 
 class LabOrderCommand(BaseCommand):
@@ -19,6 +25,50 @@ class LabOrderCommand(BaseCommand):
     diagnosis_codes: list[str] = []
     fasting_required: bool = False
     comment: str | None = None
+
+    def _get_error_details(
+        self, method: Literal["originate", "edit", "delete", "commit", "enter_in_error"]
+    ) -> list[InitErrorDetails]:
+        errors = super()._get_error_details(method)
+
+        lab_partner_obj = None
+        if self.lab_partner:
+            lab_partner_obj = (
+                LabPartner.objects.filter(Q(name=self.lab_partner) | Q(id=self.lab_partner))
+                .values("id")
+                .first()
+            )
+            if not lab_partner_obj:
+                errors.append(
+                    self._create_error_detail(
+                        "value",
+                        f"lab partner with Id or Name {self.lab_partner} not found",
+                        self.lab_partner,
+                    )
+                )
+
+        if self.tests_order_codes:
+            if not lab_partner_obj:
+                errors.append(
+                    self._create_error_detail(
+                        "value", "lab partner is required to find tests", self.tests_order_codes
+                    )
+                )
+            else:
+                tests = LabPartnerTest.objects.filter(
+                    Q(order_code__in=self.tests_order_codes) | Q(id__in=self.tests_order_codes),
+                    lab_vendor_id=lab_partner_obj["id"],
+                )
+                if tests.count() != len(self.tests_order_codes):
+                    errors.append(
+                        self._create_error_detail(
+                            "value",
+                            f"tests with order codes {self.tests_order_codes} not found",
+                            self.tests_order_codes,
+                        )
+                    )
+
+        return errors
 
     @property
     def values(self) -> dict:
