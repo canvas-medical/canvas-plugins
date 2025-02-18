cdc5c28318ab7af0a07ea48c6fc39257c5322834
Michela Iannaccone <mbiannaccone@gmail.com>
2025-02-18 10:17:31 -0500

    create effects for billinglineitems

---
+++ canvas_sdk/effects/billing_line_item/__init__.py
@@ -0,0 +1,11 @@
+from canvas_sdk.effects.billing_line_item.add_billing_line_item import (
+    AddBillingLineItem,
+)
+from canvas_sdk.effects.billing_line_item.remove_billing_line_item import (
+    RemoveBillingLineItem,
+)
+from canvas_sdk.effects.billing_line_item.update_billing_line_item import (
+    UpdateBillingLineItem,
+)
+
+__all__ = ("AddBillingLineItem", "UpdateBillingLineItem", "RemoveBillingLineItem")
---
+++ canvas_sdk/effects/billing_line_item/remove_billing_line_item.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from canvas_sdk.effects.base import EffectType, _BaseEffect
+
+
+class RemoveBillingLineItem(_BaseEffect):
+    """
+    An Effect that will remove a billing line item in a note footer.
+    """
+
+    class Meta:
+        effect_type = EffectType.REMOVE_BILLING_LINE_ITEM
+        apply_required_fields = ("billing_line_item_id",)
+
+    billing_line_item_id: str | None = None
+
+    @property
+    def effect_payload(self) -> dict[str, Any]:
+        """The payload of the effect."""
+        return {
+            "billing_line_item_id": self.billing_line_item_id,
+        }
