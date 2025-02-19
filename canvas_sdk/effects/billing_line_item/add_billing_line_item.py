d8b91876478f07771097f841aa015bdc8128f3df
Michela Iannaccone <mbiannaccone@gmail.com>
2025-02-19 11:01:22 -0500

    feat: create effects for billinglineitems (#418)
    Signed-off-by: Michela Iannaccone <mbiannaccone@gmail.com>

---
+++ canvas_sdk/effects/billing_line_item/add_billing_line_item.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+from canvas_sdk.commands.constants import Coding
+from canvas_sdk.effects.base import EffectType, _BaseEffect
+
+
+class AddBillingLineItem(_BaseEffect):
+    """
+    An Effect that will result in a billing line item in a note footer.
+    """
+
+    class Meta:
+        effect_type = EffectType.ADD_BILLING_LINE_ITEM
+        apply_required_fields = ("note_id", "cpt")
+
+    note_id: str | None = None
+    cpt: str | None = None
+    units: int | None = 1
+    assessment_ids: list[str] = []
+    modifiers: list[Coding] = []
+
+    @property
+    def values(self) -> dict[str, Any]:
+        """The BillingLineItem's values."""
+        return {
+            "cpt": self.cpt,
+            "units": self.units,
+            "assessment_ids": self.assessment_ids,
+            "modifiers": self.modifiers,
+        }
+
+    @property
+    def effect_payload(self) -> dict[str, Any]:
+        """The payload of the effect."""
+        return {
+            "note_id": self.note_id,
+            "data": self.values,
+        }
---
+++ canvas_sdk/effects/billing_line_item/update_billing_line_item.py
@@ -0,0 +1,44 @@
+from typing import Any
+
+from canvas_sdk.commands.constants import Coding
+from canvas_sdk.effects.base import EffectType, _BaseEffect
+
+
+class UpdateBillingLineItem(_BaseEffect):
+    """
+    An Effect that will update a billing line item in a note footer.
+    """
+
+    class Meta:
+        effect_type = EffectType.UPDATE_BILLING_LINE_ITEM
+        apply_required_fields = ("billing_line_item_id",)
+
+    billing_line_item_id: str | None = None
+    cpt: str | None = None
+    units: int | None = None
+    assessment_ids: list[str] | None = None
+    modifiers: list[Coding] | None = None
+
+    @property
+    def values(self) -> dict[str, Any]:
+        """The BillingLineItem's values."""
+        values: dict[str, str | int | list] = {}
+        # only include the values where a value has been set so as to not unintentionally override existing values
+        if self.cpt is not None:
+            values["cpt"] = self.cpt
+        if self.units is not None:
+            values["units"] = self.units
+        if self.assessment_ids is not None:
+            values["assessment_ids"] = self.assessment_ids
+        if self.modifiers is not None:
+            values["modifiers"] = self.modifiers
+
+        return values
+
+    @property
+    def effect_payload(self) -> dict[str, Any]:
+        """The payload of the effect."""
+        return {
+            "billing_line_item_id": self.billing_line_item_id,
+            "data": self.values,
+        }
