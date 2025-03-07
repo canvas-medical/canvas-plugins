a104352815b773f4529cc7ca17ee5e133810ec16
Christopher Sande <christopher.sande@canvasmedical.com>
2025-03-07 13:26:38 -0600

    Pass path pattern to Request __init__

--- canvas_sdk/tests/handlers/test_simple_api.py
+++ canvas_sdk/tests/handlers/test_simple_api.py
@@ -1,4 +1,5 @@
 import json
+import re
 from base64 import b64decode, b64encode
 from collections.abc import Callable, Iterable, Mapping, Sequence
 from http import HTTPStatus
@@ -242,7 +243,8 @@ def test_request(
     path = "/route"
     query_string = "value1=a&value2=b"
     request = Request(
-        make_event(EventType.SIMPLE_API_REQUEST, method, path, query_string, body, headers)
+        make_event(EventType.SIMPLE_API_REQUEST, method, path, query_string, body, headers),
+        re.compile(path),
     )
 
     assert request.method == method
