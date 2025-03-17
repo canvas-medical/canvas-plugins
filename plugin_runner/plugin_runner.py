262645bf0d12eea4bf65244170eec570ad249486
Beau Gunderson <beau@beaugunderson.com>
2025-03-17 15:50:32 -0600

    don't add JWTs as a secret

--- plugin_runner/plugin_runner.py
+++ plugin_runner/plugin_runner.py
@@ -29,11 +29,7 @@ from canvas_sdk.events import Event, EventRequest, EventResponse, EventType
 from canvas_sdk.protocols import ClinicalQualityMeasure
 from canvas_sdk.utils.stats import get_duration_ms, statsd_client
 from logger import log
-from plugin_runner.authentication import (
-    ontologies_token_for_plugin,
-    science_token_for_plugin,
-    token_for_plugin,
-)
+from plugin_runner.authentication import token_for_plugin
 from plugin_runner.installation import install_plugins
 from plugin_runner.sandbox import Sandbox
 from settings import (
@@ -174,11 +170,7 @@ class PluginRunner(PluginRunnerServicer):
             secrets = plugin.get("secrets", {})
 
             secrets.update(
-                {
-                    "graphql_jwt": token_for_plugin(plugin_name=plugin_name, audience="home"),
-                    "science_jwt": science_token_for_plugin(plugin_name=plugin_name),
-                    "ontologies_jwt": ontologies_token_for_plugin(plugin_name=plugin_name),
-                }
+                {"graphql_jwt": token_for_plugin(plugin_name=plugin_name, audience="home")}
             )
 
             try:
