d1c2bdb099de088e1023558924e6c40d12f76507
Andrew Duane <andrew.duane@canvasmedical.com>
2025-03-24 17:02:55 -0700

    Expose auth_user email in data module

--- canvas_sdk/v1/data/user.py
+++ canvas_sdk/v1/data/user.py
@@ -9,3 +9,4 @@ class CanvasUser(models.Model):
         db_table = "canvas_sdk_data_api_auth_user_001"
 
     dbid = models.BigIntegerField(db_column="dbid", primary_key=True)
+    email = models.CharField()
