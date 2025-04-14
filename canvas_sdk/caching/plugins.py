8591313f1f34abd828d01d3172223af820d9556b
Beau Gunderson <beau@beaugunderson.com>
2025-04-14 14:02:31 -0600

    improve module import security and add tests to enforce it

--- canvas_sdk/caching/plugins.py
+++ canvas_sdk/caching/plugins.py
@@ -1,13 +1,17 @@
-from typing import Any
+from __future__ import annotations
 
-import settings
-from canvas_sdk.caching.base import Cache
-from canvas_sdk.caching.client import get_cache as get_cache_client
+from typing import TYPE_CHECKING, Any
+
+from canvas_sdk.caching.client import get_cache as _get_cache_client
 from canvas_sdk.utils.plugins import plugin_only
+from settings import CANVAS_SDK_CACHE_TIMEOUT_SECONDS
+
+if TYPE_CHECKING:
+    from canvas_sdk.caching.base import Cache
 
 
 @plugin_only
 def get_cache(**kwargs: Any) -> Cache:
     """Get the cache client for plugins."""
     prefix = kwargs["plugin_name"]
-    return get_cache_client("plugins", prefix, settings.CANVAS_SDK_CACHE_TIMEOUT_SECONDS)
+    return _get_cache_client("plugins", prefix, CANVAS_SDK_CACHE_TIMEOUT_SECONDS)
