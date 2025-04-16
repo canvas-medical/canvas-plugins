c8f86c1d3b1ff10e84484dd3c3d9d2a36209f24a
Beau Gunderson <beau@beaugunderson.com>
2025-04-16 13:18:51 -0600

    add script to load all plugins for testing purposes

--- plugin_runner/plugin_runner.py
+++ plugin_runner/plugin_runner.py
@@ -465,7 +465,7 @@ def get_client() -> tuple[redis.Redis, redis.client.PubSub]:
     return client, pubsub
 
 
-def load_or_reload_plugin(path: pathlib.Path) -> None:
+def load_or_reload_plugin(path: pathlib.Path) -> bool:
     """Given a path, load or reload a plugin."""
     log.info(f'Loading plugin at "{path}"')
 
@@ -481,11 +481,11 @@ def load_or_reload_plugin(path: pathlib.Path) -> None:
         log.error(f'Unable to load plugin "{name}": {e}')
         sentry_sdk.capture_exception(e)
 
-        return
+        return False
 
     secrets_file = path / SECRETS_FILE_NAME
-
     secrets_json = {}
+
     if secrets_file.exists():
         try:
             secrets_json = json.load(secrets_file.open())
@@ -502,7 +502,9 @@ def load_or_reload_plugin(path: pathlib.Path) -> None:
         log.error(f'Unable to load plugin "{name}": {str(e)}')
         sentry_sdk.capture_exception(e)
 
-        return
+        return False
+
+    any_failed = False
 
     for handler in handlers:
         # TODO add class colon validation to existing schema validation
@@ -513,6 +515,8 @@ def load_or_reload_plugin(path: pathlib.Path) -> None:
         except ValueError as e:
             log.error(f'Unable to parse class for plugin "{name}": "{handler["class"]}"')
             sentry_sdk.capture_exception(e)
+
+            any_failed = True
 
             continue
 
@@ -544,6 +548,10 @@ def load_or_reload_plugin(path: pathlib.Path) -> None:
                 log.error(error_line)
 
             sentry_sdk.capture_exception(e)
+
+            any_failed = True
+
+    return not any_failed
 
 
 def refresh_event_type_map() -> None:
