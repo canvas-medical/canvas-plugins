d1b938b6a773c1326037c02c3970545bffaf310f
Rui Batista <rui.batista@canvasmedical.com>
2025-04-22 15:38:57 +0100

    add chart section review command

---
+++ canvas_sdk/commands/commands/chart_section_review.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+from canvas_sdk.commands.base import _BaseCommand
+
+
+class ChartSectionReviewCommand(_BaseCommand):
+    """A class for managing a Chart Section Review command within a specific note."""
+
+    class Meta:
+        key = "chartSectionReview"
+        commit_required_fields = ("section",)
+
+    class Sections(Enum):
+        CONDITIONS = "conditions"
+        SURGICAL_HISTORY = "surgical_history"
+        MEDICATIONS = "medications"
+        FAMILY_HISTORY = "family_histories"
+        ALLERGIES = "allergies"
+        IMMUNIZATIONS = "immunizations"
+
+    section: Sections
