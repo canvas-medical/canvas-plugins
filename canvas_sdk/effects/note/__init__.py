4a9b0c6e857ddfcaef52af9dc9ad152ba19ab051
Nuno Silva <nuno.silva@canvasmedical.com>
2025-05-01 13:50:27 +0100

    feat: add create note and create appointment effects (#518)

---
+++ canvas_sdk/effects/note/__init__.py
@@ -0,0 +1,10 @@
+from canvas_sdk.effects.note.appointment import Appointment, ScheduleEvent
+from canvas_sdk.effects.note.base import AppointmentIdentifier
+from canvas_sdk.effects.note.note import Note
+
+__all__ = __exports__ = (
+    "AppointmentIdentifier",
+    "Note",
+    "Appointment",
+    "ScheduleEvent",
+)
