30f669a954ce50e3fcb76e45b643b30e8d5bc090
dejan-predojevic-vicert <dejan.predojevic@vicert.com>
2025-05-13 14:24:27 +0200

    Refactoring plugins

--- test-plugins/supervising_provider_prescribe/README.md
+++ test-plugins/supervising_provider_prescribe/README.md
@@ -3,7 +3,14 @@ supervising_provider_prescribe
 
 ## Description
 
-Fills the supervising provider field in the Prescribe command.
+This protocol responds to the PRESCRIBE_COMMAND__POST_ORIGINATE event.
+
+It is used to test whether the supervising provider field is automatically populated
+when the Prescribe command is triggered. The protocol reacts to the command's creation
+and sets the field accordingly.
+
+The same logic can be tested for Refill and Adjust Prescription commands by updating
+the RESPONDS_TO event and the command class.
 
 ### Important Note!
 
--- test-plugins/supervising_provider_prescribe/protocols/my_protocol.py
+++ test-plugins/supervising_provider_prescribe/protocols/my_protocol.py
@@ -1,14 +1,21 @@
-from canvas_generated.messages.effects_pb2 import Effect
 from canvas_sdk.commands import PrescribeCommand
+from canvas_sdk.effects import Effect
 from canvas_sdk.events import EventType
 from canvas_sdk.protocols import BaseProtocol
+from canvas_sdk.v1.data import Staff
 from logger import log
 
 
 class Protocol(BaseProtocol):
     """
     This protocol responds to the PRESCRIBE_COMMAND__POST_ORIGINATE event.
-    When a prescribe command is created, protocol will be triggered and fill in the supervising provider field.
+
+    It is used to test whether the supervising provider field is automatically populated
+    when the Prescribe command is triggered. The protocol reacts to the command's creation
+    and sets the field accordingly.
+
+    The same logic can be tested for Refill and Adjust Prescription commands by updating
+    the RESPONDS_TO event and the command class.
     """
 
     RESPONDS_TO = EventType.Name(EventType.PRESCRIBE_COMMAND__POST_ORIGINATE)
@@ -17,9 +24,14 @@ class Protocol(BaseProtocol):
         """This method gets called when an event of the type RESPONDS_TO is fired."""
         log.info(f"Target {self.target} on patient {self.context['patient']['id']}")
 
+        staff = Staff.objects.first()
+        if not staff:
+            log.warning("No staff found — skipping update.")
+            return []
+
         prescription = PrescribeCommand(
             command_uuid=str(self.target),
-            supervising_provider_id="4150cd20de8a470aa570a852859ac87e",
+            supervising_provider_id=staff.id,
         )
 
         return [prescription.edit()]
