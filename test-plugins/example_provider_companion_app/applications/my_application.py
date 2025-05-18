6170320bd0e69302bd0cf574aa4d415ddbd25a63
Andrew Duane <andrew.duane@canvasmedical.com>
2025-05-17 18:09:41 -0700

    docs: add example application plugins (#622)

---
+++ test-plugins/example_provider_companion_app/applications/my_application.py
@@ -0,0 +1,24 @@
+from canvas_sdk.effects import Effect
+from canvas_sdk.effects.launch_modal import LaunchModalEffect
+from canvas_sdk.handlers.application import Application
+
+
+class MyApplication(Application):
+    """An embeddable application that can be registered to Canvas."""
+
+    def on_open(self) -> Effect:
+        """Handle the on_open event."""
+        # Implement this method to handle the application on_open event.
+        # You can look up data here to be used in knowing what to launch, if
+        # what you're launching depends on some dynamic criteria.
+
+        return LaunchModalEffect(
+            # This URL is what will get iframed. It can be hosted elsewhere,
+            # or it could be hosted by your plugin! Canvas plugins can serve
+            # html, css, js, or json.
+            #
+            # If embedding a remote URL, be sure to declare it in the URL
+            # permissions section of your plugin's CANVAS_MANIFEST.json
+            url="/plugin-io/api/example_provider_companion_app/app/provider-app",
+            target=LaunchModalEffect.TargetType.DEFAULT_MODAL,
+        ).apply()
