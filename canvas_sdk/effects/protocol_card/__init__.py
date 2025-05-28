39067e1228278548fb7177babde23360b0308f86
Michela Iannaccone <mbiannaccone@gmail.com>
2025-05-28 15:44:52 -0400

    get rid of snoozeprotocol effect

--- canvas_sdk/effects/protocol_card/__init__.py
+++ canvas_sdk/effects/protocol_card/__init__.py
@@ -1,4 +1,6 @@
 from canvas_sdk.effects.protocol_card.protocol_card import ProtocolCard, Recommendation
-from canvas_sdk.effects.protocol_card.snooze_protocol_card import SnoozeProtocolCard
 
-__all__ = __exports__ = ("ProtocolCard", "Recommendation", "SnoozeProtocolCard")
+__all__ = __exports__ = (
+    "ProtocolCard",
+    "Recommendation",
+)
