f1d8d2d5f4df8bfa5f9189c378544db6e22e294e
Miguel Quintas <miguel.quintas@canvasmedical.com>
2025-06-04 10:32:07 +0100

    feat: add is_portal_registered field to user

--- canvas_sdk/v1/data/user.py
+++ canvas_sdk/v1/data/user.py
@@ -12,6 +12,7 @@ class CanvasUser(models.Model):
     email = models.EmailField(db_column="email")
     phone_number = models.CharField(db_column="phone_number", max_length=255)
     last_invite_date_time = models.DateTimeField()
+    is_portal_registered = models.BooleanField()
 
 
 __exports__ = ("CanvasUser",)
