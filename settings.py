22fa5e4b5b0833c644ed3ef57efef6d89954b984
Beau Gunderson <beau@beaugunderson.com>
2025-06-04 22:57:59 -0600

    add thread_cleanup

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import sys
 from pathlib import Path
@@ -19,6 +20,16 @@ IS_SCRIPT = env_to_bool("IS_SCRIPT", "plugin_runner.py" not in sys.argv[0])
 PLUGIN_POOL_DEBUG = env_to_bool("PLUGIN_POOL_DEBUG")
 CUSTOMER_IDENTIFIER = os.getenv("CUSTOMER_IDENTIFIER", "local")
 APP_NAME = os.getenv("APP_NAME")
+
+if PLUGIN_POOL_DEBUG:
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("[%(process)d:%(thread)d] %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    pool_logger = logging.getLogger("psycopg.pool")
+    pool_logger.setLevel(logging.DEBUG)
+    pool_logger.addHandler(handler)
 
 INTEGRATION_TEST_URL = os.getenv("INTEGRATION_TEST_URL")
 INTEGRATION_TEST_CLIENT_ID = os.getenv("INTEGRATION_TEST_CLIENT_ID")
