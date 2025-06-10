bf3d0a23dab5fe40cbb872c92a7c599345a5db93
Michela Iannaccone <mbiannaccone@gmail.com>
2025-06-10 10:27:42 -0400

    required coding field on instruct command for commit

--- canvas_sdk/commands/commands/instruct.py
+++ canvas_sdk/commands/commands/instruct.py
@@ -10,6 +10,7 @@ class InstructCommand(BaseCommand):
 
     class Meta:
         key = "instruct"
+        commit_required_fields = ("coding",)
 
     coding: Coding | None = Field(default=None, json_schema_extra={"commands_api_name": "instruct"})
     comment: str | None = None
