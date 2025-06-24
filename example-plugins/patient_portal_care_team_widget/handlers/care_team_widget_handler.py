d102fdd3610cd2b26cf7c278cd02204a7b36bba4
Rui Lima <rui.lima@canvasmedical.com>
2025-06-24 15:56:48 +0100

    Resolve with suggestions

--- example-plugins/patient_portal_care_team_widget/handlers/care_team_widget_handler.py
+++ example-plugins/patient_portal_care_team_widget/handlers/care_team_widget_handler.py
@@ -3,8 +3,7 @@ from canvas_sdk.effects.widgets import PortalWidget
 from canvas_sdk.events import EventType
 from canvas_sdk.handlers.base import BaseHandler
 from canvas_sdk.templates import render_to_string
-from canvas_sdk.v1.data.care_team import CareTeamMembershipStatus
-from canvas_sdk.v1.data.patient import Patient
+from canvas_sdk.v1.data.care_team import CareTeamMembership, CareTeamMembershipStatus
 
 
 # Inherit from BaseHandler to properly get registered for events
@@ -16,18 +15,27 @@ class CareTeamWidgetHandler(BaseHandler):
 
     def compute(self) -> list[Effect]:
         """This method gets called when an event of the type RESPONDS_TO is fired."""
-        patient = Patient.objects.get(id=self.target)
-        patient_care_team = patient.care_team_memberships.filter(status=CareTeamMembershipStatus.ACTIVE)
+        patient_care_team = CareTeamMembership.objects.values(
+            "staff__first_name",
+            "staff__last_name",
+            "staff__prefix",
+            "staff__suffix",
+            "staff__photos__url",
+            "role_display",
+        ).filter(
+            patient__id=self.target,
+            status=CareTeamMembershipStatus.ACTIVE,
+        )
 
         care_team = []
         for member in patient_care_team:
-            name = f"{member.staff.first_name} {member.staff.last_name}"
-            prefixed_name = f"{member.staff.prefix} " if member.staff.prefix else "" + f"{name}"
-            professional_name = f"{prefixed_name}, {member.staff.suffix}" if member.staff.suffix else prefixed_name
-            photo_url = member.staff.photo_url
-            role = member.role
+            # Aliasing the member's name components for clarity
+            name = f"{member['staff__first_name']} {member['staff__last_name']}"
+            prefixed_name = f"{member['staff__prefix']} " if member['staff__prefix'] else "" + f"{name}"
+            professional_name = f"{prefixed_name}, {member['staff__suffix']}" if member['staff__suffix'] else prefixed_name
+            photo_url = member['staff__photos__url']
+            role = member['role_display']
 
-            # Prefetch the photo for each care team member
             care_team.append(
                 {
                     "name": name,
