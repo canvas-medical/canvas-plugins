a6487e1a79f1a888f72f8b834084de54ec579757
Michela Iannaccone <mbiannaccone@gmail.com>
2025-06-26 15:23:29 -0400

    create new staff events

--- canvas_generated/messages/events_pb2.pyi
+++ canvas_generated/messages/events_pb2.pyi
@@ -55,6 +55,8 @@ class EventType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     PRESCRIPTION_UPDATED: _ClassVar[EventType]
     REFERRAL_REPORT_CREATED: _ClassVar[EventType]
     REFERRAL_REPORT_UPDATED: _ClassVar[EventType]
+    STAFF_CREATED: _ClassVar[EventType]
+    STAFF_UPDATED: _ClassVar[EventType]
     TASK_COMMENT_CREATED: _ClassVar[EventType]
     TASK_CREATED: _ClassVar[EventType]
     TASK_LABELS_ADJUSTED: _ClassVar[EventType]
@@ -91,6 +93,8 @@ class EventType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     TASK_COMPLETED: _ClassVar[EventType]
     DETECTED_ISSUE_EVIDENCE_CREATED: _ClassVar[EventType]
     DETECTED_ISSUE_EVIDENCE_UPDATED: _ClassVar[EventType]
+    STAFF_ACTIVATED: _ClassVar[EventType]
+    STAFF_DEACTIVATED: _ClassVar[EventType]
     PRE_COMMAND_ORIGINATE: _ClassVar[EventType]
     POST_COMMAND_ORIGINATE: _ClassVar[EventType]
     PRE_COMMAND_UPDATE: _ClassVar[EventType]
@@ -888,6 +892,8 @@ PRESCRIPTION_CREATED: EventType
 PRESCRIPTION_UPDATED: EventType
 REFERRAL_REPORT_CREATED: EventType
 REFERRAL_REPORT_UPDATED: EventType
+STAFF_CREATED: EventType
+STAFF_UPDATED: EventType
 TASK_COMMENT_CREATED: EventType
 TASK_CREATED: EventType
 TASK_LABELS_ADJUSTED: EventType
@@ -924,6 +930,8 @@ TASK_CLOSED: EventType
 TASK_COMPLETED: EventType
 DETECTED_ISSUE_EVIDENCE_CREATED: EventType
 DETECTED_ISSUE_EVIDENCE_UPDATED: EventType
+STAFF_ACTIVATED: EventType
+STAFF_DEACTIVATED: EventType
 PRE_COMMAND_ORIGINATE: EventType
 POST_COMMAND_ORIGINATE: EventType
 PRE_COMMAND_UPDATE: EventType
