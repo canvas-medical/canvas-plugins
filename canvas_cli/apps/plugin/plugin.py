c517cb983f6dc28496ea46cdb95ce4cdb90baf32
Mary Dickson <mary.dickson@canvasmedical.com>
2025-06-30 16:51:10 -0700

    Update canvas_cli/apps/plugin/plugin.py
    Co-authored-by: Andrew Duane <andrew.duane@canvasmedical.com>
    Signed-off-by: Mary Dickson <mdickson@gmail.com>

--- canvas_cli/apps/plugin/plugin.py
+++ canvas_cli/apps/plugin/plugin.py
@@ -296,7 +296,7 @@ def enable(
         raise typer.Exit(1) from None
 
     if r.ok:
-        print(f"Plugin {name} installation requested! Check logs for more details.")
+        print(f"Plugin {name} new version uploaded! Check logs for more details.")
     else:
         print(f"Status code {r.status_code}: {r.text}")
         raise typer.Exit(1)
