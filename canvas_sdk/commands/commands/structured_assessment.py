6593a8f401572dacb05f68580da9e89f820955c9
Nuno Silva <nuno.silva@canvasmedical.com>
2025-07-02 11:06:37 +0100

    implement question toggles for questionnaire based commands

--- canvas_sdk/commands/commands/structured_assessment.py
+++ canvas_sdk/commands/commands/structured_assessment.py
@@ -1,7 +1,8 @@
 from canvas_sdk.commands.commands.questionnaire import QuestionnaireCommand
+from canvas_sdk.commands.commands.questionnaire.toggle_questions import ToggleQuestionsMixin
 
 
-class StructuredAssessmentCommand(QuestionnaireCommand):
+class StructuredAssessmentCommand(ToggleQuestionsMixin, QuestionnaireCommand):
     """A class for managing physical exam command."""
 
     class Meta:
