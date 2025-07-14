2a82b7437b1b27d7c9a76a97431991147b7d97fc
Rui Lima <rui.lima@canvasmedical.com>
2025-07-14 15:07:27 +0100

    Prescription modal and form working

--- example-plugins/patient_portal_plugin/handlers/medications_handler.py
+++ example-plugins/patient_portal_plugin/handlers/medications_handler.py
@@ -1,14 +1,13 @@
+import json
 from http import HTTPStatus
 
-from canvas_sdk.commands.commands.prescribe import PrescribeCommand
-from canvas_sdk.commands.commands.refill import RefillCommand
-from canvas_sdk.commands.constants import ClinicalQuantity
 from canvas_sdk.effects import Effect
 from canvas_sdk.effects.launch_modal import LaunchModalEffect
 from canvas_sdk.effects.simple_api import JSONResponse, Response
 from canvas_sdk.handlers.simple_api.api import SimpleAPIRoute
 from canvas_sdk.handlers.simple_api.security import PatientSessionAuthMixin
-from canvas_sdk.v1.data.medication import Medication, Status
+from canvas_sdk.templates.utils import render_to_string
+from canvas_sdk.v1.data.medication import Medication
 from canvas_sdk.v1.data.patient import Patient
 from logger import log
 
@@ -16,6 +15,8 @@ from logger import log
 class MedicationsHandler(PatientSessionAuthMixin, SimpleAPIRoute):
     """Handler for the Medications plugin."""
     PATH = "/medications/<id>"
+
+    DEFAULT_BACKGROUND_COLOR = "#17634d"
 
     def get(self) -> list[Response | Effect]:
         """Serve the main HTML page for the Medications plugin."""
@@ -27,24 +28,34 @@ class MedicationsHandler(PatientSessionAuthMixin, SimpleAPIRoute):
             return [JSONResponse({"error": "Patient ID is required"}, status_code=HTTPStatus.BAD_REQUEST)]
 
         try:
-            medications = Medication.objects.filter(
-                patient__id=internal_patient_id,
-                deleted=False,
-                status=Status.ACTIVE,
-            ).order_by("-start_date")
-            medications_ids = [str(med.id) for med in medications]
+            medications = Medication.objects.for_patient(internal_patient_id)
+
+            for medication in medications:
+                for coding in medication.codings.all():
+                    log.info(f"system:  {coding.system}")
+                    log.info(f"code:    {coding.code}")
+                    log.info(f"display: {coding.display}")
+
         except Patient.DoesNotExist:
             return [JSONResponse({"error": "Patient not found"}, status_code=HTTPStatus.NOT_FOUND)]
+        except Medication.DoesNotExist:
+            return [JSONResponse({"error": "No medications found for this patient"}, status_code=HTTPStatus.NOT_FOUND)]
 
         return [
             JSONResponse({
                 "message": "Welcome to the Medications plugin!",
                 "patient_id": internal_patient_id,
-                "medications": medications_ids,
+                # "medications": medications,
             }, status_code=HTTPStatus.OK),
             LaunchModalEffect(
-                url="http://localhost:8000/app/messaging",
-                content=None,
+                content=render_to_string(
+                    "templates/refill_form.html",
+                    {
+                        "patient_id": internal_patient_id,
+                        "medications": [medication.codings.first() for medication in medications],
+                        "background_color": self.background_color,
+                    }
+                ),
                 target=LaunchModalEffect.TargetType.DEFAULT_MODAL,
                 title="Medications Information",
                 patient_id=internal_patient_id,
@@ -69,28 +80,12 @@ class MedicationsHandler(PatientSessionAuthMixin, SimpleAPIRoute):
 
     def _refill(self) -> Response:
         """Handle medication refill requests."""
-        command = RefillCommand(
-            fdb_code="216092",
-            icd10_codes=["R51"],
-            sig="Take one tablet daily after meals",
-            days_supply=30,
-            quantity_to_dispense=30,
-            type_to_dispense=ClinicalQuantity(
-                representative_ndc="12843016128",
-                ncpdp_quantity_qualifier_code="C48542"
-            ),
-            refills=3,
-            substitutions=PrescribeCommand.Substitutions.ALLOWED,
-            pharmacy="Main Street Pharmacy",
-            prescriber_id="provider_123",
-            supervising_provider_id="provider_456",
-            note_to_pharmacist="Please verify patient's insurance before processing."
-        )
+        return JSONResponse({
+            "message": "Medication refill request processed successfully.",
+        }, status_code=HTTPStatus.OK)
 
-        return JSONResponse(
-            {
-                "message": "Medication refill request processed successfully.",
-            },
-            status_code=HTTPStatus.OK
-        )
+    @property
+    def background_color(self) -> str:
+        """Get the background color from secrets, defaulting to a specific color if not set."""
+        return self.secrets.get("BACKGROUND_COLOR") or self.DEFAULT_BACKGROUND_COLOR
 
