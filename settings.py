b48cafb5cbe80888827a52de0ff8e93822929c4f
José Magalhães <jose.magalhaes@canvasmedical.com>
2025-07-21 11:21:39 +0100

    fix: open sqlite db in read-only mode

--- settings.py
+++ settings.py
@@ -96,7 +96,10 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": str(BASE_DIR / "db.sqlite3"),
+            "NAME": f"file:{str(BASE_DIR / 'db.sqlite3')}?mode=ro",
+            "OPTIONS": {
+                "uri": True,
+            },
         }
     }
 
