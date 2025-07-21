f78b40212dc5b48d445a36432c6dca7d37755586
Beau Gunderson <beau@beaugunderson.com>
2025-07-21 15:38:10 -0600

    update build configuration

--- hatch_build.py
+++ hatch_build.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
-ALLOWED_MODULE_IMPORTS_PATH = "./plugin_runner/allowed-module-imports.pickle"
+ALLOWED_MODULE_IMPORTS_PATH = "plugin_runner/allowed-module-imports.pickle"
 
 
 class CustomBuildHook(BuildHookInterface):
@@ -16,10 +16,10 @@ class CustomBuildHook(BuildHookInterface):
         Verify that the command to generate the allow modules file has been run
         before the build is allowed.
         """
-        allowed_modules_file = pathlib.Path(ALLOWED_MODULE_IMPORTS_PATH)
+        allowed_modules_path = pathlib.Path(__file__).parent / ALLOWED_MODULE_IMPORTS_PATH
 
-        if not allowed_modules_file.exists():
+        if not allowed_modules_path.exists():
             raise Exception(
                 f"'{ALLOWED_MODULE_IMPORTS_PATH}' does not exist; run the "
-                "generation command (`uv run python -m plugin_runner.generate_allowed_modules`)"
+                "generation command (`uv run python -m plugin_runner.generate_allowed_imports`)"
             )
