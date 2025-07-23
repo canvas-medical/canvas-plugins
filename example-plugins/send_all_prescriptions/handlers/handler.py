d5c7d5207e4e2f9c3c645c0ff5d4678a4a12d9f5
Rui Batista <rui.batista@canvasmedical.com>
2025-07-23 14:42:41 +0100

    add plugin to send all prescription in a note via action button

---
+++ example-plugins/send_all_prescriptions/handlers/handler.py
@@ -0,0 +1,25 @@
+from canvas_sdk.commands import PrescribeCommand
+from canvas_sdk.effects import Effect
+from canvas_sdk.handlers.action_button import ActionButton
+from canvas_sdk.v1.data import Command
+
+
+class SendPrescriptionButtonHandler(ActionButton):
+    BUTTON_TITLE = "Send Prescriptions"
+    BUTTON_KEY = "SEND_ALL_PRESCRIPTIONS"
+    BUTTON_LOCATION = ActionButton.ButtonLocation.NOTE_FOOTER
+
+
+    def handle(self) -> list[Effect]:
+        note_id = self.context.get("note_id")
+
+        effects = []
+        # get all committed prescribe commands
+        prescribe_commands = Command.objects.filter(note_id=note_id, schema_key="prescribe", committer__isnull=False)
+
+        for command in prescribe_commands:
+            prescribe = PrescribeCommand()
+            prescribe.command_uuid = str(command.id)
+            effects.append(prescribe.send())
+
+        return effects
