69ec215d675a5abdb938e12de429b878be8126ad
Rui Batista <rui.batista@canvasmedical.com>
2025-08-04 08:28:51 -0700

    add patient id to patient order tracking html add patient id to api filtering updated global order tracking with new dropdown styles

--- example-plugins/order_tracking/applications/patient_order_tracking_app.py
+++ example-plugins/order_tracking/applications/patient_order_tracking_app.py
@@ -2,13 +2,15 @@ from canvas_sdk.effects import Effect
 from canvas_sdk.effects.launch_modal import LaunchModalEffect
 from canvas_sdk.handlers.application import Application
 from canvas_sdk.templates import render_to_string
+from logger import log
 
 
 class PatientOrderTrackingApplication(Application):
     """An embeddable application that can be registered to Canvas."""
 
     def on_open(self) -> Effect:
+        patient_id = self.context["patient"]["id"]
         return LaunchModalEffect(
             target=LaunchModalEffect.TargetType.RIGHT_CHART_PANE,
-            content=render_to_string("templates/patient_worklist_orders.html"),
+            content=render_to_string("templates/patient_worklist_orders.html", context={"patientId": patient_id}),
         ).apply()
