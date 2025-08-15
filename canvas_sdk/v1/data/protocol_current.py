94a8289ffbc179cf1c34f9e723579cd5df5eac1f
Rui Lima <rui.lima@canvasmedical.com>
2025-08-15 19:12:55 +0100

    Ruff

--- canvas_sdk/v1/data/protocol_current.py
+++ canvas_sdk/v1/data/protocol_current.py
@@ -10,7 +10,9 @@ class ProtocolCurrent(ProtocolResult, IdentifiableModel):
     class Meta:
         db_table = "canvas_sdk_data_api_protocolcurrent_001"
 
-    patient = models.ForeignKey("v1.Patient", on_delete=models.DO_NOTHING, related_name="protocol_currents")
+    patient = models.ForeignKey(
+        "v1.Patient", on_delete=models.DO_NOTHING, related_name="protocol_currents"
+    )
     result_hash = models.CharField(max_length=32, default="")
     snooze_date = models.DateField(null=True)
 
