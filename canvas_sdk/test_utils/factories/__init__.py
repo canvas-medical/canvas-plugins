ccfe93003fe7318e796df28e58ccb0993c3e1fa8
Rui Lima <rui.lima@canvasmedical.com>
2025-09-08 10:22:06 +0100

    Missing factories export

--- canvas_sdk/test_utils/factories/__init__.py
+++ canvas_sdk/test_utils/factories/__init__.py
@@ -1,4 +1,12 @@
-from .patient import PatientAddressFactory, PatientFactory
+from canvas_sdk.test_utils.factories.facility import FacilityFactory
+
+from .patient import PatientAddressFactory, PatientFacilityAddressFactory, PatientFactory
 from .user import CanvasUserFactory
 
-__all__ = ("CanvasUserFactory", "PatientAddressFactory", "PatientFactory")
+__all__ = (
+    "CanvasUserFactory",
+    "FacilityFactory",
+    "PatientAddressFactory",
+    "PatientFacilityAddressFactory",
+    "PatientFactory",
+)
