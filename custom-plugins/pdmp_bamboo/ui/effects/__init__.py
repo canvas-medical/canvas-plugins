49c9732222e2e21af360304342f4c9f44b7d9116
MirkoPopovicVicert <mirko.popovic@vicert.com>
2025-09-19 16:28:52 +0200

    delete code

--- custom-plugins/pdmp_bamboo/ui/effects/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-UI Effects Package
-
-Contains services for creating Canvas effects like structured assessments.
-"""
-
-from pdmp_bamboo.ui.effects.assessment_effects import AssessmentEffectsService
-
-__all__ = ['AssessmentEffectsService']
