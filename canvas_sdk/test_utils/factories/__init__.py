52e1ba0ce969a80b69d1a15dd5640a61053c8c7d
Michela Iannaccone <mbiannaccone@gmail.com>
2025-09-23 12:47:36 -0400

    remove factories to be done later

--- canvas_sdk/test_utils/factories/__init__.py
+++ canvas_sdk/test_utils/factories/__init__.py
@@ -1,10 +1,6 @@
 from .facility import FacilityFactory
-from .medication import MedicationFactory
-from .medication_statement import MedicationStatementFactory
-from .note import NoteFactory, NoteTypeFactory
 from .patient import PatientAddressFactory, PatientFacilityAddressFactory, PatientFactory
 from .protocol_current import ProtocolCurrentFactory
-from .stop_medication_event import StopMedicationEventFactory
 from .user import CanvasUserFactory
 
 __all__ = (
@@ -14,9 +10,4 @@ __all__ = (
     "PatientFacilityAddressFactory",
     "PatientFactory",
     "ProtocolCurrentFactory",
-    "StopMedicationEventFactory",
-    "NoteFactory",
-    "NoteTypeFactory",
-    "MedicationFactory",
-    "MedicationStatementFactory",
 )
