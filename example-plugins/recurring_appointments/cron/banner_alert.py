0234ccbf6cde1e09f91bcf818811f628190bc823
Rui Batista <103107926+ruibatistacanvas@users.noreply.github.com>
2025-09-24 16:26:02 +0100

    chore(docs): recurrence example plugin (#1057)

---
+++ example-plugins/recurring_appointments/cron/banner_alert.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from django.db.models import Q, Count
+
+from canvas_sdk.effects import Effect
+from canvas_sdk.effects.banner_alert import AddBannerAlert
+from canvas_sdk.handlers.cron_task import CronTask
+from canvas_sdk.v1.data import Appointment
+
+
+class CheckPatientAppointmentRecurrence(CronTask):
+    """Every day at 3 AM, check for recurring appointments that only have <= 1 visit left and add a banner alert to the patient timeline."""
+    SCHEDULE = "0 3 * * *"
+
+    def execute(self) -> list[Effect]:
+        """Get all recurring appointments that have == 1 visit left and add a banner alert to the patient timeline."""
+
+        _filter = Q(children__start_time__gt=datetime.now()) & ~Q(children__status="cancelled")
+        patients_to_add_banners = Appointment.objects.filter(_filter).annotate(future_appointments=Count('children', filter=_filter)).values_list("patient__id", flat=True).filter(future_appointments=1).distinct()
+
+        effects = []
+        for patient in patients_to_add_banners:
+            banner = AddBannerAlert(
+                patient_id=patient,
+                key="recurring-appointment-end-banner",
+                narrative="Only 1 visit scheduled. Confirm future recurrence and schedule",
+                placement=[
+                    AddBannerAlert.Placement.TIMELINE,
+                ],
+                intent=AddBannerAlert.Intent.INFO
+            )
+            effects.append(banner.apply())
+
+        return effects
