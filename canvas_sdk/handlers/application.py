a348889873eeb8b254db6519dd045e7f7e9243a5
Michela Iannaccone <mbiannaccone@gmail.com>
2025-09-29 13:45:29 -0400

    allow an application handler to return multiple effects

--- canvas_sdk/handlers/application.py
+++ canvas_sdk/handlers/application.py
@@ -15,23 +15,34 @@ class Application(BaseHandler, ABC):
 
     def compute(self) -> list[Effect]:
         """Handle the application events."""
+        if self.event.target.id != self.identifier:
+            return []
         match self.event.type:
             case EventType.APPLICATION__ON_OPEN:
-                return [self.on_open()] if self.event.target.id == self.identifier else []
+                open_effect_or_effects: Effect | list[Effect] = self.on_open()
+                if type(open_effect_or_effects) is list[Effect]:
+                    return open_effect_or_effects
+                if type(open_effect_or_effects) is Effect:
+                    return [open_effect_or_effects]
+                return []
             case EventType.APPLICATION__ON_CONTEXT_CHANGE:
-                if self.event.target.id == self.identifier:
-                    effect = self.on_context_change()
-                    return [effect] if effect is not None else []
+                context_effect_or_effects: Effect | list[Effect] | None = self.on_context_change()
+                if context_effect_or_effects is None:
+                    return []
+                if type(context_effect_or_effects) is list[Effect]:
+                    return context_effect_or_effects
+                if type(context_effect_or_effects) is Effect:
+                    return [context_effect_or_effects]
                 return []
             case _:
                 return []
 
     @abstractmethod
-    def on_open(self) -> Effect:
+    def on_open(self) -> Effect | list[Effect]:
         """Handle the application open event."""
         ...
 
-    def on_context_change(self) -> Effect | None:
+    def on_context_change(self) -> Effect | list[Effect] | None:
         """Handle the application context change event."""
         return None
 
