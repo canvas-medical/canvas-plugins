3173af977d6b737e906e2615fb872064921b0cc3
Rui Batista <rui.batista@canvasmedical.com>
2025-10-03 14:59:08 +0100

    add free text support for family history, medication statement, past surgical history and perform

--- canvas_sdk/commands/commands/family_history.py
+++ canvas_sdk/commands/commands/family_history.py
@@ -1,4 +1,7 @@
+from pydantic_core import InitErrorDetails
+
 from canvas_sdk.commands.base import _BaseCommand as BaseCommand
+from canvas_sdk.commands.constants import CodeSystems, Coding
 
 
 class FamilyHistoryCommand(BaseCommand):
@@ -7,9 +10,22 @@ class FamilyHistoryCommand(BaseCommand):
     class Meta:
         key = "familyHistory"
 
-    family_history: str | None = None
+    family_history: str | Coding | None = None
     relative: str | None = None
     note: str | None = None
+
+    def _get_error_details(self, method: str) -> list[InitErrorDetails]:
+        errors = super()._get_error_details(method)
+
+        if (
+            isinstance(self.family_history, dict)
+            and self.family_history["system"] != CodeSystems.SNOMED
+            and self.family_history["system"] != CodeSystems.UNSTRUCTURED
+        ):
+            message = f"The 'coding.system' field must be '{CodeSystems.SNOMED}' or '{CodeSystems.UNSTRUCTURED}'."
+            errors.append(self._create_error_detail("value", message, self.family_history))
+
+        return errors
 
 
 __exports__ = ("FamilyHistoryCommand",)
--- canvas_sdk/commands/commands/past_surgical_history.py
+++ canvas_sdk/commands/commands/past_surgical_history.py
@@ -1,8 +1,10 @@
 from datetime import date
 
 from pydantic import Field
+from pydantic_core import InitErrorDetails
 
 from canvas_sdk.commands.base import _BaseCommand as BaseCommand
+from canvas_sdk.commands.constants import CodeSystems, Coding
 
 
 class PastSurgicalHistoryCommand(BaseCommand):
@@ -11,9 +13,22 @@ class PastSurgicalHistoryCommand(BaseCommand):
     class Meta:
         key = "surgicalHistory"
 
-    past_surgical_history: str | None = None
+    past_surgical_history: str | Coding | None = None
     approximate_date: date | None = None
     comment: str | None = Field(max_length=1000, default=None)
+
+    def _get_error_details(self, method: str) -> list[InitErrorDetails]:
+        errors = super()._get_error_details(method)
+
+        if (
+            isinstance(self.past_surgical_history, dict)
+            and self.past_surgical_history["system"] != CodeSystems.SNOMED
+            and self.past_surgical_history["system"] != CodeSystems.UNSTRUCTURED
+        ):
+            message = f"The 'coding.system' field must be '{CodeSystems.SNOMED}' or '{CodeSystems.UNSTRUCTURED}'."
+            errors.append(self._create_error_detail("value", message, self.past_surgical_history))
+
+        return errors
 
 
 __exports__ = ("PastSurgicalHistoryCommand",)
