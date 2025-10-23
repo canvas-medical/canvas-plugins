817dcf382452c5aa5962cef00fdad1b24f1dd91b
Michela Iannaccone <mbiannaccone@gmail.com>
2025-10-23 10:42:12 -0400

    create move claim to queue effect and tests

--- canvas_generated/messages/effects_pb2.pyi
+++ canvas_generated/messages/effects_pb2.pyi
@@ -221,6 +221,7 @@ class EffectType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     ADD_BILLING_LINE_ITEM: _ClassVar[EffectType]
     UPDATE_BILLING_LINE_ITEM: _ClassVar[EffectType]
     REMOVE_BILLING_LINE_ITEM: _ClassVar[EffectType]
+    MOVE_CLAIM_TO_QUEUE: _ClassVar[EffectType]
     SHOW_PATIENT_PORTAL_MENU_ITEMS: _ClassVar[EffectType]
     PORTAL_WIDGET: _ClassVar[EffectType]
     LAUNCH_MODAL: _ClassVar[EffectType]
@@ -490,6 +491,7 @@ PATIENT_PORTAL__APPLICATION_CONFIGURATION: EffectType
 ADD_BILLING_LINE_ITEM: EffectType
 UPDATE_BILLING_LINE_ITEM: EffectType
 REMOVE_BILLING_LINE_ITEM: EffectType
+MOVE_CLAIM_TO_QUEUE: EffectType
 SHOW_PATIENT_PORTAL_MENU_ITEMS: EffectType
 PORTAL_WIDGET: EffectType
 LAUNCH_MODAL: EffectType
---
+++ canvas_sdk/effects/claim_queue.py
@@ -0,0 +1,53 @@
+from typing import Any
+from uuid import UUID
+
+from pydantic_core import InitErrorDetails
+
+from canvas_sdk.effects.base import EffectType, _BaseEffect
+from canvas_sdk.v1.data import Claim, ClaimQueue
+
+
+class MoveClaimToQueue(_BaseEffect):
+    """
+    An Effect that moves a Claim to a Queue.
+    """
+
+    class Meta:
+        effect_type = EffectType.MOVE_CLAIM_TO_QUEUE
+
+    claim_id: UUID | str
+    queue: str
+
+    @property
+    def values(self) -> dict[str, Any]:
+        """The claim_id and queue_id."""
+        return {"claim_id": self.claim_id, "queue": self.queue}
+
+    @property
+    def effect_payload(self) -> dict[str, Any]:
+        """The payload of the effect."""
+        return {"data": self.values}
+
+    def _get_error_details(self, method: Any) -> list[InitErrorDetails]:
+        errors = super()._get_error_details(method)
+
+        if not Claim.objects.filter(id=self.claim_id).exists():
+            errors.append(
+                self._create_error_detail(
+                    "value",
+                    "Claim does not exist",
+                    self.claim_id,
+                )
+            )
+        if not ClaimQueue.objects.filter(name=self.queue).exists():
+            errors.append(
+                self._create_error_detail(
+                    "value",
+                    "Queue does not exist",
+                    self.queue,
+                )
+            )
+        return errors
+
+
+__exports__ = ("MoveClaimToQueue",)
