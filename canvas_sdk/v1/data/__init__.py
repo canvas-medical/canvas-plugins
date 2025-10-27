f881445405d17d3eae6313685bb484de166049f2
Miguel Quintas <miguel.quintas@canvasmedical.com>
2025-10-27 21:55:32 +0000

    feat: add calendar model

--- canvas_sdk/v1/data/__init__.py
+++ canvas_sdk/v1/data/__init__.py
@@ -9,6 +9,7 @@ from .assessment import Assessment
 from .banner_alert import BannerAlert
 from .billing import BillingLineItem, BillingLineItemModifier
 from .business_line import BusinessLine
+from .calendar import Calendar
 from .care_team import CareTeamMembership, CareTeamRole
 from .charge_description_master import ChargeDescriptionMaster
 from .claim import Claim, ClaimCoverage, ClaimPatient, ClaimQueue, InstallmentPlan
@@ -127,6 +128,7 @@ __all__ = __exports__ = (
     "BillingLineItemModifier",
     "BusinessLine",
     "BulkPatientPosting",
+    "Calendar",
     "CanvasUser",
     "CareTeamMembership",
     "CareTeamRole",
