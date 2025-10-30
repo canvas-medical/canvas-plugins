cc6e4da1a4241852a3d4f1636f7e223c07c0ee37
Michela Iannaccone <mbiannaccone@gmail.com>
2025-10-30 09:58:47 -0400

    feat: claim label effects (#1179)
    Signed-off-by: Michela Iannaccone <mbiannaccone@gmail.com>

--- canvas_generated/messages/effects_pb2.pyi
+++ canvas_generated/messages/effects_pb2.pyi
@@ -224,6 +224,8 @@ class EffectType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     ADD_BILLING_LINE_ITEM: _ClassVar[EffectType]
     UPDATE_BILLING_LINE_ITEM: _ClassVar[EffectType]
     REMOVE_BILLING_LINE_ITEM: _ClassVar[EffectType]
+    ADD_CLAIM_LABEL: _ClassVar[EffectType]
+    REMOVE_CLAIM_LABEL: _ClassVar[EffectType]
     UPDATE_CLAIM_LINE_ITEM: _ClassVar[EffectType]
     SHOW_PATIENT_PORTAL_MENU_ITEMS: _ClassVar[EffectType]
     PORTAL_WIDGET: _ClassVar[EffectType]
@@ -498,6 +500,8 @@ REMOVE_APPOINTMENT_LABEL: EffectType
 ADD_BILLING_LINE_ITEM: EffectType
 UPDATE_BILLING_LINE_ITEM: EffectType
 REMOVE_BILLING_LINE_ITEM: EffectType
+ADD_CLAIM_LABEL: EffectType
+REMOVE_CLAIM_LABEL: EffectType
 UPDATE_CLAIM_LINE_ITEM: EffectType
 SHOW_PATIENT_PORTAL_MENU_ITEMS: EffectType
 PORTAL_WIDGET: EffectType
