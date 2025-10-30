6d9044ae53330525371793d633b7819702764e4d
Miguel Quintas <miguel.quintas@canvasmedical.com>
2025-10-30 11:43:24 +0000

    feat: add update and delete events
    ^ Conflicts:
    ^	canvas_generated/messages/effects_pb2.pyi
    ^	protobufs/canvas_generated/messages/effects.proto

--- canvas_sdk/v1/data/__init__.py
+++ canvas_sdk/v1/data/__init__.py
@@ -33,6 +33,7 @@ from .detected_issue import DetectedIssue, DetectedIssueEvidence
 from .device import Device
 from .discount import Discount
 from .encounter import Encounter
+from .event import Event
 from .facility import Facility
 from .goal import Goal
 from .imaging import ImagingOrder, ImagingReport, ImagingReview
@@ -166,6 +167,7 @@ __all__ = __exports__ = (
     "Discount",
     "EligibilitySummary",
     "Encounter",
+    "Event",
     "Facility",
     "Goal",
     "ImagingOrder",
