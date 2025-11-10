20233538dd612a1d1f5c045f213c035ac6ea72ff
Michela Iannaccone <michela.iannaccone@canvasmedical.com>
2025-11-10 13:53:49 -0500

    create validations for claims remit

--- canvas_sdk/effects/payment/base.py
+++ canvas_sdk/effects/payment/base.py
@@ -5,9 +5,12 @@ from enum import Enum
 from typing import Any, Literal
 from uuid import UUID
 
+from django.db.models import QuerySet
 from pydantic import Field
+from pydantic_core import InitErrorDetails
 
 from canvas_sdk.effects.base import _BaseEffect
+from canvas_sdk.v1.data import ClaimCoverage, ClaimLineItem
 
 
 class PaymentMethod(Enum):
@@ -47,6 +50,24 @@ class PostPaymentBase(_BaseEffect):
             "total_collected": str(self.total_collected),
         }
 
+    def validate_payment_method_fields(self) -> list[InitErrorDetails]:
+        """Checks that check_number and check_date are provided when method is 'check'."""
+        if self.method != PaymentMethod.CHECK:
+            return []
+        if not self.check_number:
+            return [
+                self._create_error_detail(
+                    "value", "Check number is required for payment method CHECK", self.check_number
+                )
+            ]
+        if not self.check_date:
+            return [
+                self._create_error_detail(
+                    "value", "Check date is required for payment method CHECK", self.check_date
+                )
+            ]
+        return []
+
 
 @dataclass
 class LineItemTransaction:
@@ -75,3 +96,181 @@ class LineItemTransaction:
             # can only be the patient or other active coverages on the claim
             "write_off": self.write_off,
         }
+
+    def is_first_transaction_for_line_item(
+        self, line_item_transactions: list["LineItemTransaction"], index: int
+    ) -> bool:
+        """Returns True if this transaction is the first transaction for the claim line item, which can have many."""
+        for i, lit in enumerate(line_item_transactions):
+            if lit.claim_line_item_id == self.claim_line_item_id:
+                return i == index
+        return False
+
+    def is_allowed_valid(self, is_patient_pmt: bool) -> list[tuple[str, str, Any]]:
+        """Checks if allowed amount present on a patient payment."""
+        if is_patient_pmt and self.allowed:
+            return [self._format_error("Allowed amount should be $0 or None for patient postings")]
+        return []
+
+    def is_payment_required(
+        self, is_first_transaction_for_line_item: bool
+    ) -> list[tuple[str, str, Any]]:
+        """Checks that the first transaction for a claim line item is either a payment or adjustment."""
+        if (
+            self.payment is None
+            and is_first_transaction_for_line_item
+            and self.adjustment is None
+            and self.allowed is None
+        ):
+            return [
+                self._format_error(
+                    "Payment or adjustment is required for a claim line item's first transaction"
+                )
+            ]
+        return []
+
+    def is_adjustment_required(
+        self, is_first_line_item_transaction: bool
+    ) -> list[tuple[str, str, Any]]:
+        """Checks that sequential transactions for a claim line item have an adjustment, and also checks if the adjustments have the correct corresponding fields."""
+        errors = []
+
+        if not is_first_line_item_transaction and not self.adjustment:
+            errors.append(
+                self._format_error(
+                    "Specify an adjustment amount for added adjustments or remove the added adjustment line for this claim line item"
+                )
+            )
+        if not self.adjustment and self.adjustment_code:
+            errors.append(
+                self._format_error("Enter an adjustment amount for the specified adjustment type")
+            )
+        if not self.adjustment and self.transfer_remaining_balance_to:
+            errors.append(self._format_error("Enter an adjustment amount to transfer"))
+        if not self.adjustment and self.write_off:
+            errors.append(self._format_error("Enter an adjustment amount to write off"))
+
+        return errors
+
+    def is_adjustment_type_required(self) -> list[tuple[str, str, Any]]:
+        """Checks if there is an adjustment without an adjustment code."""
+        if self.adjustment and not self.adjustment_code:
+            return [self._format_error("Specify an adjustment code for the adjustment amount")]
+        return []
+
+    def is_transfer_to_required(self) -> list[tuple[str, str, Any]]:
+        """Checks if a transfer_to is required."""
+        if not self.adjustment or not self.adjustment_code:
+            return []
+        adjustment_type = self.adjustment_code.split("-")
+        # When "Transfer" adjustment group/code is selected a transfer destination is required.
+        # This is the only group/code with this requirement.
+        if (
+            len(adjustment_type) < 2
+            and not self.transfer_remaining_balance_to
+            and adjustment_type[0] == "Transfer"
+        ):
+            return [self._format_error("Specify a payer to transfer the adjusted amount")]
+        return []
+
+    def is_adjustment_allowed(self, is_self_copay_line_item: bool) -> list[tuple[str, str, Any]]:
+        """Checks if the adjustment is accurately formed."""
+        errors = []
+
+        if is_self_copay_line_item and any(
+            [
+                self.adjustment,
+                self.adjustment_code,
+                self.write_off,
+                self.transfer_remaining_balance_to,
+            ]
+        ):
+            errors.append(
+                self._format_error("Adjustments and transfers not allowed for COPAY charges")
+            )
+
+        if self.adjustment and self.write_off and self.transfer_remaining_balance_to:
+            errors.append(
+                self._format_error(
+                    "Adjustments cannot write off and transfer at the same time, please set write off=False or transfer_remaining_balance_to=None to create the posting"
+                )
+            )
+        return errors
+
+    def is_payer_valid(
+        self, is_self_copay_line_item: bool, is_patient_pmt: bool
+    ) -> list[tuple[str, str, Any]]:
+        """Checks that payments made on a COPAY line item only come from a patient."""
+        if is_self_copay_line_item and not is_patient_pmt:
+            return [self._format_error("COPAY payments may only be posted by patients")]
+        return []
+
+    def is_transfer_to_valid(
+        self, payer_id: str | UUID | Literal["patient"], claim_coverages: QuerySet[ClaimCoverage]
+    ) -> list[tuple[str, str, Any]]:
+        """Checks that transfers are not made to the same payer as the transaction payer and that the transfer_to field is a valid payer for the claim."""
+        if not self.transfer_remaining_balance_to:
+            return []
+
+        if self.transfer_remaining_balance_to and str(self.transfer_remaining_balance_to) == str(
+            payer_id
+        ):
+            return [self._format_error("Can't create transfers to same payer")]
+
+        if (
+            self.transfer_remaining_balance_to != "patient"
+            and not claim_coverages.filter(payer_id=self.transfer_remaining_balance_to).exists()
+        ):
+            return [
+                self._format_error(
+                    "Balance can only be transferred to patient or an active coverage payer_id for the claim"
+                )
+            ]
+        return []
+
+    def _format_error(self, error_message: str) -> tuple[str, str, Any]:
+        return ("value", error_message, self.to_dict())
+
+    def validate(
+        self,
+        line_item_transactions: list["LineItemTransaction"],
+        index: int,
+        claim_line_items: QuerySet[ClaimLineItem],
+        payer_id: str | UUID | Literal["patient"],
+        claim_coverages: QuerySet[ClaimCoverage],
+    ) -> list[tuple[str, str, Any]]:
+        """Returns error details for a line item transaction, using the context of other transactions for the claim."""
+        if not (line_item := claim_line_items.filter(id=self.claim_line_item_id).first()):
+            return [
+                self._format_error(
+                    "The provided claim_line_item_id does not correspond to an existing ClaimLineItem"
+                )
+            ]
+
+        is_patient_pmt = payer_id == "patient"
+        if errors := self.is_allowed_valid(is_patient_pmt):
+            return errors
+
+        is_first_transaction_for_line_item = self.is_first_transaction_for_line_item(
+            line_item_transactions, index
+        )
+        if errors := self.is_payment_required(is_first_transaction_for_line_item):
+            return errors
+        if errors := self.is_adjustment_required(is_first_transaction_for_line_item):
+            return errors
+
+        if errors := self.is_adjustment_type_required():
+            return errors
+        if errors := self.is_transfer_to_required():
+            return errors
+
+        is_self_copay_line_item = line_item.proc_code == "COPAY"
+        if errors := self.is_adjustment_allowed(is_self_copay_line_item):
+            return errors
+        if errors := self.is_payer_valid(is_self_copay_line_item, is_patient_pmt):
+            return errors
+
+        if errors := self.is_transfer_to_valid(payer_id, claim_coverages):
+            return errors
+
+        return []
--- canvas_sdk/effects/payment/post_claims_remit.py
+++ canvas_sdk/effects/payment/post_claims_remit.py
@@ -1,15 +1,16 @@
 import base64
 import mimetypes
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Literal
 from uuid import UUID
 
+from django.db.models import QuerySet
 from pydantic import FilePath
 from pydantic_core import InitErrorDetails
 
 from canvas_sdk.effects.base import EffectType
 from canvas_sdk.effects.payment.base import LineItemTransaction, PaymentMethod, PostPaymentBase
-from canvas_sdk.v1.data import Claim
+from canvas_sdk.v1.data import Claim, ClaimCoverage
 
 
 @dataclass
@@ -18,7 +19,7 @@ class ClaimAllocation:
 
     claim_id: str | UUID
     line_item_transactions: list[LineItemTransaction]
-    subscriber_id: str | None = None
+    subscriber_number: str | None = None
     move_to_queue_name: str | None = None
     description: str | None = None
 
@@ -26,11 +27,56 @@ class ClaimAllocation:
         """Convert dataclass to dictionary."""
         return {
             "claim_id": str(self.claim_id),
-            "subscriber_id": self.subscriber_id,
+            "subscriber_number": self.subscriber_number,
             "line_item_transactions": [lit.to_dict() for lit in self.line_item_transactions],
             "move_to_queue_name": self.move_to_queue_name,
             "description": self.description,
         }
+
+    def check_payer_id(
+        self, payer_id: str | UUID | Literal["patient"], claim_coverages: QuerySet[ClaimCoverage]
+    ) -> list[tuple[str, str, Any]]:
+        """Checks that the payer is either 'patient' or an active coverage for the claim."""
+        if payer_id == "patient":
+            return []
+        coverages = claim_coverages.filter(payer_id=payer_id)
+        if not coverages:
+            return [
+                (
+                    "value",
+                    f"No active coverage with payer_id {payer_id} for this claim",
+                    {"claim_id": self.claim_id},
+                )
+            ]
+        if coverages.count() > 1 and self.subscriber_number:
+            coverages = coverages.filter(subscriber_number=self.subscriber_number)
+            if not coverages.exists():
+                return [
+                    (
+                        "value",
+                        f"No active coverage with payer_id {payer_id} and subscriber_number {self.subscriber_number} for this claim",
+                        {"claim_id": self.claim_id},
+                    )
+                ]
+        return []
+
+    def validate(
+        self, payer_id: str | UUID | Literal["patient"], claim: Claim
+    ) -> list[tuple[str, str, str]]:
+        """Returns error details for a claim allocation."""
+        claim_line_items = claim.line_items.active()
+        claim_coverages = claim.coverages.active()
+
+        if errors := self.check_payer_id(payer_id, claim_coverages):
+            return errors
+
+        for index, line_item_transaction in enumerate(self.line_item_transactions):
+            if errors := line_item_transaction.validate(
+                self.line_item_transactions, index, claim_line_items, payer_id, claim_coverages
+            ):
+                return errors
+
+        return []
 
 
 class PostClaimsRemit(PostPaymentBase):
@@ -41,55 +87,80 @@ class PostClaimsRemit(PostPaymentBase):
     class Meta:
         effect_type = EffectType.POST_CLAIMS_REMIT
 
-    era_document: FilePath
+    era_document: FilePath | None = None
     claims_allocation: list[ClaimAllocation]
 
     @property
-    def values(self) -> dict[str, Any]:
-        """The values for the payload."""
+    def era_file(self) -> dict[str, str] | None:
+        """The base64 encoded file contents."""
+        if not self.era_document:
+            return None
         with open(self.era_document, "rb") as fh:
             b64 = base64.b64encode(fh.read()).decode()
         content_type = mimetypes.guess_type(str(self.era_document))[0] or "application/octet-stream"
         return {
+            "filename": self.era_document.name,
+            "content_type": content_type,
+            "base64": b64,
+        }
+
+    @property
+    def values(self) -> dict[str, Any]:
+        """The values for the payload."""
+        return {
             "posting": {
                 "payer_id": str(self.payer_id),
-                "era_file": {
-                    "filename": self.era_document.name,
-                    "content_type": content_type,
-                    "base64": b64,
-                },
+                "era_file": self.era_file,
             },
             "payment_collection": self.payment_collection_values,
             "claims_allocation": [c.to_dict() for c in self.claims_allocation],
         }
 
-    def _get_error_details(self, method: Any) -> list[InitErrorDetails]:
-        errors = super()._get_error_details(method)
-
+    def validate_payment_method(self) -> list[InitErrorDetails]:
+        """Enforces that only Check or Other are chosen as payment method."""
         if self.method not in [PaymentMethod.CHECK, PaymentMethod.OTHER]:
-            errors.append(
-                self._create_error_detail(
-                    "value",
-                    "Claims remits must have payment method of 'check' or 'other'",
-                    self.method.value,
+            return [
+                (
+                    self._create_error_detail(
+                        "value",
+                        "Claims remits can only have payment method of 'check' or 'other'",
+                        self.method.value,
+                    )
                 )
-            )
+            ]
+        return []
 
-        claim_ids = [str(c.claim_id) for c in self.claims_allocation]
-        claims = Claim.objects.filter(id__in=claim_ids)
-        if claims.count() < len(claim_ids):
-            incorrect_claim_ids = set(claim_ids) - {
-                str(e) for e in claims.values_list("externally_exposable_id", flat=True)
-            }
-            errors.append(
+    def validate_claim_ids(
+        self, claim_ids: list[str], claims: QuerySet[Claim]
+    ) -> list[InitErrorDetails]:
+        """Checks that all claim_ids provided correspond to an existing Claim."""
+        if claims.count() == len(claim_ids):
+            return []
+        incorrect_claim_ids = set(claim_ids) - {str(e) for e in claims.values_list("id", flat=True)}
+        return [
+            (
                 self._create_error_detail(
                     "value",
-                    f"There are {len(incorrect_claim_ids)} claim_ids without an existing Claim",
+                    f"There are {len(incorrect_claim_ids)} claim_ids that do not correspond to an existing Claim",
                     ", ".join(incorrect_claim_ids),
                 )
             )
+        ]
 
-        # todo: more validation!
+    def _get_error_details(self, method: Any) -> list[InitErrorDetails]:
+        errors = super()._get_error_details(method)
+
+        errors.extend(self.validate_payment_method())
+        errors.extend(self.validate_payment_method_fields())
+
+        claim_ids = [str(c.claim_id) for c in self.claims_allocation]
+        claims = Claim.objects.filter(id__in=claim_ids)
+        errors.extend(self.validate_claim_ids(claim_ids, claims))
+
+        for claim_allocation in self.claims_allocation:
+            claim = claims.get(id=claim_allocation.claim_id)
+            claim_errors = claim_allocation.validate(self.payer_id, claim)
+            errors.extend([self._create_error_detail(*e) for e in claim_errors])
 
         return errors
 
