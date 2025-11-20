56300785191f83ebef58f5d42a77c1cefee1e71d
José Magalhães <jose.magalhaes@canvasmedical.com>
2025-11-20 17:29:19 +0000

    feat: add support for note lock, sign and unlock effects
    # Conflicts:
    #	canvas_generated/messages/effects_pb2.py
    #	canvas_sdk/test_utils/factories/__init__.py

--- canvas_sdk/effects/note/note.py
+++ canvas_sdk/effects/note/note.py
@@ -1,15 +1,31 @@
 import datetime
 import json
-from typing import Any
+from typing import Any, cast
 from uuid import UUID
 
 from pydantic_core import InitErrorDetails
 
+from canvas_generated.messages.effects_pb2 import EffectType
 from canvas_sdk.effects import Effect
 from canvas_sdk.effects.note.base import NoteOrAppointmentABC
 from canvas_sdk.v1.data import Note as NoteModel
 from canvas_sdk.v1.data import NoteType, Patient
-from canvas_sdk.v1.data.note import NoteTypeCategories
+from canvas_sdk.v1.data.note import NoteStates, NoteTypeCategories
+
+TRANSITION_STATE_MATRIX = {
+    NoteStates.NEW: [NoteStates.LOCKED, NoteStates.PUSHED],
+    NoteStates.LOCKED: [NoteStates.SIGNED, NoteStates.UNLOCKED],
+    NoteStates.UNLOCKED: [NoteStates.LOCKED, NoteStates.PUSHED],
+    NoteStates.SIGNED: [NoteStates.UNLOCKED, NoteStates.SIGNED],
+    NoteStates.PUSHED: [NoteStates.LOCKED, NoteStates.PUSHED],
+}
+
+ACTION_STATE_MATRIX = {
+    "lock": NoteStates.LOCKED,
+    "unlock": NoteStates.UNLOCKED,
+    "sign": NoteStates.SIGNED,
+    "push_charges": NoteStates.PUSHED,
+}
 
 
 class Note(NoteOrAppointmentABC):
@@ -34,9 +50,59 @@ class Note(NoteOrAppointmentABC):
         """Pushes BillingLineItems from the Note to the associated Claim. Identicial to clicking the Push Charges button in the note footer."""
         self._validate_before_effect("push_charges")
         return Effect(
-            type="PUSH_NOTE_CHARGES",
+            type=EffectType.PUSH_NOTE_CHARGES,
             payload=json.dumps({"note": self.instance_id}),
         )
+
+    def unlock(self) -> Effect:
+        """Unlocks the note to allow further edits."""
+        self._validate_before_effect("unlock")
+        return Effect(
+            type=EffectType.UNLOCK_NOTE,
+            payload=json.dumps({"note": self.instance_id}),
+        )
+
+    def lock(self) -> Effect:
+        """Locks the note to prevent further edits."""
+        self._validate_before_effect("lock")
+        return Effect(
+            type=EffectType.LOCK_NOTE,
+            payload=json.dumps({"note": self.instance_id}),
+        )
+
+    def sign(self) -> Effect:
+        """Signs the note."""
+        self._validate_before_effect("sign")
+        return Effect(
+            type=EffectType.SIGN_NOTE,
+            payload=json.dumps({"note": self.instance_id}),
+        )
+
+    def _validate_state_transition(
+        self, note: NoteModel, next_state: NoteStates
+    ) -> tuple[bool, InitErrorDetails | None]:
+        """Validates state transitions for the note."""
+        current_state = note.current_state.state if note.current_state else None
+
+        if not current_state:
+            return False, self._create_error_detail(
+                "value", "Unsupported state transitions", next_state
+            )
+
+        if next_state not in TRANSITION_STATE_MATRIX.get(cast(NoteStates, current_state), []):
+            return False, self._create_error_detail(
+                "value",
+                f"Invalid state transition from {current_state} to {next_state}.",
+                next_state,
+            )
+
+        is_sig_required = note.note_type_version.is_sig_required
+        if next_state == NoteStates.SIGNED and not is_sig_required:
+            return False, self._create_error_detail(
+                "value", "Cannot sign a note that does not require a signature.", next_state
+            )
+
+        return True, None
 
     def _get_error_details(self, method: Any) -> list[InitErrorDetails]:
         """
@@ -50,15 +116,16 @@ class Note(NoteOrAppointmentABC):
         """
         errors = super()._get_error_details(method)
 
-        if method == "push_charges":
+        if method in ACTION_STATE_MATRIX:
             if not self.instance_id:
                 errors.append(
                     self._create_error_detail(
                         "missing",
-                        "Field 'instance_id' is required to push charges from a note to a claim.",
+                        "Field 'instance_id' is required.",
                         None,
                     )
                 )
+                return errors
             elif not (note := NoteModel.objects.filter(id=self.instance_id).first()):
                 errors.append(
                     self._create_error_detail(
@@ -67,7 +134,24 @@ class Note(NoteOrAppointmentABC):
                         self.instance_id,
                     )
                 )
-            elif not note.note_type_version or not note.note_type_version.is_billable:
+                return errors
+
+            if note.note_type_version.category in (
+                NoteTypeCategories.LETTER,
+                NoteTypeCategories.MESSAGE,
+            ):
+                errors.append(
+                    self._create_error_detail(
+                        "value",
+                        f"Note with note type {note.note_type_version.name} cannot perform action '{method}'.",
+                        note.note_type_version,
+                    )
+                )
+                return errors
+
+            if method == "push_charges" and (
+                not note.note_type_version or not note.note_type_version.is_billable
+            ):
                 errors.append(
                     self._create_error_detail(
                         "value",
@@ -75,6 +159,13 @@ class Note(NoteOrAppointmentABC):
                         note.note_type_version,
                     )
                 )
+                return errors
+
+            if method in ACTION_STATE_MATRIX:
+                _, error = self._validate_state_transition(note, ACTION_STATE_MATRIX[method])
+
+                if error:
+                    errors.append(error)
 
             return errors
 
