9a9993369b4304c58fe2c43cb44cc1c854d8f76b
Joe Wilson <joe.wilson@canvasmedical.com>
2025-11-24 11:21:52 -0800

    feat: adds PracticeLocationAddress and allowed imports for PDMP plugin (#1190)
    Co-authored-by: Reba Magier <reba.magier@canvasmedical.com>

--- canvas_sdk/test_utils/factories/practicelocation.py
+++ canvas_sdk/test_utils/factories/practicelocation.py
@@ -1,6 +1,9 @@
+from datetime import date
+
 import factory
 
-from canvas_sdk.v1.data import PracticeLocation, PracticeLocationSetting
+from canvas_sdk.v1.data import PracticeLocation, PracticeLocationAddress, PracticeLocationSetting
+from canvas_sdk.v1.data.common import AddressState, AddressType, AddressUse
 
 
 class PracticeLocationSettingFactory(factory.django.DjangoModelFactory[PracticeLocationSetting]):
@@ -32,3 +35,28 @@ class PracticeLocationFactory(factory.django.DjangoModelFactory[PracticeLocation
     tax_id_type = "E"
     npi_number = "1234567890"
     group_npi_number = "112233445"
+
+
+class PracticeLocationAddressFactory(factory.django.DjangoModelFactory[PracticeLocationAddress]):
+    """Factory for PracticeLocationAddress."""
+
+    class Meta:
+        model = PracticeLocationAddress
+
+    practice_location = factory.SubFactory(
+        "canvas_sdk.test_utils.factories.PracticeLocationFactory"
+    )
+    line1 = "1234 Golden Gate Ave."
+    line2 = "Suite 456"
+    city = "San Francisco"
+    district = ""
+    state_code = "CA"
+    postal_code = "94100"
+    use = AddressUse.WORK
+    type = AddressType.BOTH
+    longitude = -122.476944
+    latitude = 37.769722
+    start = date(year=2015, month=10, day=10)
+    end = None
+    country = "USA"
+    state = AddressState.ACTIVE
