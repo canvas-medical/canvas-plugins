9c0ddf527e8bf815da87f504301c3fc3a9593880
David Jantzen <david.jantzen@canvasmedical.com>
2025-12-10 21:17:53 -0800

    float to dec for safety, partitions and indexes for speed

--- plugin_runner/init_plugin_schema.sql
+++ plugin_runner/init_plugin_schema.sql
@@ -1,18 +1,26 @@
 create schema if not exists {plugin_name} authorization canvas_sdk_read_only;
 
 create table if not exists {plugin_name}.custom_attribute (
-    dbid serial primary key,
-    content_type_id int,
-    object_id int,
-    name text,
+    dbid serial not null,
+    content_type_id int not null,
+    object_id int not null,
+    name varchar(256) not null,
     text_value text,
     timestamp_value timestamp with time zone,
     int_value int,
-    float_value double precision,
+    decimal_value numeric(20,10),
     bool_value boolean,
     json_value jsonb,
-    unique (content_type_id, object_id, name)
-);
+    primary key (content_type_id, object_id, name)
+) partition by list(content_type_id);
+
+create index on {plugin_name}.custom_attribute (dbid);
+create index on {plugin_name}.custom_attribute (text_value) where text_value is not null;
+create index on {plugin_name}.custom_attribute (timestamp_value) where timestamp_value is not null;
+create index on {plugin_name}.custom_attribute (int_value) where int_value is not null;
+create index on {plugin_name}.custom_attribute (decimal_value) where decimal_value is not null;
+create index on {plugin_name}.custom_attribute (bool_value) where bool_value is not null;
+create index on {plugin_name}.custom_attribute using gin (json_value) where json_value is not null;
 
 grant usage on {plugin_name}.custom_attribute_dbid_seq to canvas_sdk_read_only;
 grant select on {plugin_name}.custom_attribute to canvas_sdk_read_only;
@@ -20,7 +28,7 @@ grant insert, update, delete on {plugin_name}.custom_attribute to canvas_sdk_rea
 
 create table if not exists {plugin_name}.attribute_hub (
     dbid serial primary key,
-    type varchar(255) not null
+    type varchar(100) not null
 );
 
 grant usage on {plugin_name}.attribute_hub_dbid_seq to canvas_sdk_read_only;
@@ -35,6 +43,8 @@ create table {plugin_name}.django_content_type
     model varchar(100) not null,
     unique (app_label, model)
 );
+
+grant select on {plugin_name}.django_content_type to canvas_sdk_read_only;
 
 insert into {plugin_name}.django_content_type (app_label, model) values
 ('v1', 'allergyintolerance'),
@@ -89,4 +99,22 @@ insert into {plugin_name}.django_content_type (app_label, model) values
 ('v1', 'team')
 ;
 
-grant select on {plugin_name}.django_content_type to canvas_sdk_read_only;
+
+ do $$
+  declare
+      content_type_record record;
+      partition_name text;
+  begin
+      -- loop through content types and create partitions
+      for content_type_record in
+          select id, app_label, model from {plugin_name}.django_content_type
+      loop
+          partition_name := '{plugin_name}.custom_attribute_' ||  content_type_record.app_label || '_' || content_type_record.model;
+          execute format(
+              'create table if not exists %I partition of {plugin_name}.custom_attribute for values in (%L)',
+              partition_name,
+              content_type_record.id
+          );
+      end loop;
+  end
+$$;
