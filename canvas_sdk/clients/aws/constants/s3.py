d7fcef5c2634db0e423ee391c59b58797d1255f9
dbajet <dbajet@gmail.com>
2025-12-12 18:48:12 +0000

    refactor the constant S3 as regular class

--- canvas_sdk/clients/aws/constants/s3.py
+++ canvas_sdk/clients/aws/constants/s3.py
@@ -1,7 +1,6 @@
-from dataclasses import dataclass
+from typing import Final
 
 
-@dataclass(frozen=True)
 class S3:
     """S3 client constant
     - ALGORITHM: AWS signature algorithm used (AWS4-HMAC-SHA256)
@@ -10,11 +9,11 @@ class S3:
     - UNSIGNED_PAYLOAD: Constant for unsigned payload in presigned URLs.
     """
 
-    SERVICE_NAME: str = "s3"
-    ALGORITHM: str = "AWS4-HMAC-SHA256"
-    SAFE_CHARACTERS: str = "-._~"
-    REQUEST_TYPE: str = "aws4_request"
-    UNSIGNED_PAYLOAD: str = "UNSIGNED-PAYLOAD"
+    SERVICE_NAME: Final = "s3"
+    ALGORITHM: Final = "AWS4-HMAC-SHA256"
+    SAFE_CHARACTERS: Final = "-._~"
+    REQUEST_TYPE: Final = "aws4_request"
+    UNSIGNED_PAYLOAD: Final = "UNSIGNED-PAYLOAD"
 
 
 __exports__ = ()
--- canvas_sdk/tests/clients/aws/constants/test_s3.py
+++ canvas_sdk/tests/clients/aws/constants/test_s3.py
@@ -1,21 +1,16 @@
 from canvas_sdk.clients.aws.constants.s3 import S3
-from canvas_sdk.tests.conftest import is_dataclass
+from canvas_sdk.tests.conftest import has_constants
 
 
 def test_class() -> None:
     """Test S3Item is a frozen Dataclass with correct values."""
-    assert is_dataclass(
+    assert has_constants(
         S3,
         {
-            "SERVICE_NAME": str,
-            "ALGORITHM": str,
-            "SAFE_CHARACTERS": str,
-            "REQUEST_TYPE": str,
-            "UNSIGNED_PAYLOAD": str,
+            "SERVICE_NAME": "s3",
+            "ALGORITHM": "AWS4-HMAC-SHA256",
+            "SAFE_CHARACTERS": "-._~",
+            "REQUEST_TYPE": "aws4_request",
+            "UNSIGNED_PAYLOAD": "UNSIGNED-PAYLOAD",
         },
     )
-    assert S3.SERVICE_NAME == "s3"
-    assert S3.ALGORITHM == "AWS4-HMAC-SHA256"
-    assert S3.SAFE_CHARACTERS == "-._~"
-    assert S3.REQUEST_TYPE == "aws4_request"
-    assert S3.UNSIGNED_PAYLOAD == "UNSIGNED-PAYLOAD"
