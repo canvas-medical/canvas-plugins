2457a7d1124bdc8346a768610ad814ef71d2c9f9
Rui Batista <rui.batista@canvasmedical.com>
2025-12-15 11:31:28 +0000

    add command configuration effect to customize certain commands. Add HtmlCommand

--- canvas_sdk/commands/commands/html_command.py
+++ canvas_sdk/commands/commands/html_command.py
@@ -1,18 +1,38 @@
 from typing import Any
 
-from canvas_sdk.commands.base import _BaseCommand as BaseCommand
+from pydantic_core import InitErrorDetails
+
+from canvas_sdk.commands.base import CommandConfiguration, _BaseCommand
 from canvas_sdk.commands.constants import CommandChartSection
 
 
-class HtmlCommand(BaseCommand):
+class HtmlCommand(_BaseCommand):
     """A class for managing an HTML command within a specific note."""
 
     class Meta:
         key = "htmlCommand"
 
     content: str | None = None
+    print_content: str | None = None
 
-    def configure(self, label: str, section: CommandChartSection, **kwargs: Any) -> dict[str, Any]:
+    def _get_error_details(self, method: str) -> list[InitErrorDetails]:
+        """Get error details for the HTML command."""
+        errors = super()._get_error_details(method)
+
+        if method == "originate" and not self.content:
+            errors.append(
+                self._create_error_detail(
+                    "value",
+                    "Content must be provided for an HTML command.",
+                    self.content,
+                )
+            )
+
+        return errors
+
+    def configure(
+        self, label: str, section: CommandChartSection, **kwargs: Any
+    ) -> CommandConfiguration:
         """Configure the HTML command label and section."""
         return {"key": self.Meta.key, "label": label, "section": section.value}
 
