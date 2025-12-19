18c84b836264d93de2f0ff23b1e4231d36db9f59
dbajet <dbajet@gmail.com>
2025-12-19 22:18:57 +0000

    fix typo twillio to twilio

---
+++ canvas_sdk/clients/twilio/structures/__init__.py
@@ -0,0 +1,23 @@
+from canvas_sdk.clients.twilio.structures.capabilities import Capabilities
+from canvas_sdk.clients.twilio.structures.media import Media
+from canvas_sdk.clients.twilio.structures.message import Message
+from canvas_sdk.clients.twilio.structures.phone import Phone
+from canvas_sdk.clients.twilio.structures.request_failed import RequestFailed
+from canvas_sdk.clients.twilio.structures.settings import Settings
+from canvas_sdk.clients.twilio.structures.sms_mms import SmsMms
+from canvas_sdk.clients.twilio.structures.status_inbound import StatusInbound
+from canvas_sdk.clients.twilio.structures.status_outbound_api import StatusOutboundApi
+from canvas_sdk.clients.twilio.structures.twi_ml_message import TwiMlMessage
+
+__all__ = __exports__ = (
+    "Capabilities",
+    "Media",
+    "Message",
+    "Phone",
+    "RequestFailed",
+    "Settings",
+    "SmsMms",
+    "StatusInbound",
+    "StatusOutboundApi",
+    "TwiMlMessage",
+)
