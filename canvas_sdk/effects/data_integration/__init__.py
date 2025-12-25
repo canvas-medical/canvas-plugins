8ebea64c7d47e5e02c0bef977ecabac354970deb
admir-vicert <admir.ihtijarevic@vicert.com>
2025-12-25 15:19:58 +0100

    KOALA-3914: Link document to patient effect (#1391)

--- canvas_sdk/effects/data_integration/__init__.py
+++ canvas_sdk/effects/data_integration/__init__.py
@@ -1,5 +1,15 @@
-from .remove_document_from_patient import (
-    RemoveDocumentFromPatientEffect as RemoveDocumentFromPatientEffect,
+from .link_document_to_patient import (
+    LinkDocumentConfidenceScores as LinkDocumentConfidenceScores,
 )
+from .link_document_to_patient import LinkDocumentToPatient as LinkDocumentToPatient
+from .remove_document_from_patient import (
+    RemoveDocumentConfidenceScores as RemoveDocumentConfidenceScores,
+)
+from .remove_document_from_patient import RemoveDocumentFromPatient as RemoveDocumentFromPatient
 
-__all__ = __exports__ = ("RemoveDocumentFromPatientEffect",)
+__all__ = __exports__ = (
+    "LinkDocumentConfidenceScores",
+    "LinkDocumentToPatient",
+    "RemoveDocumentConfidenceScores",
+    "RemoveDocumentFromPatient",
+)
