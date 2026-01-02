66e3a5816da95a3f2f0a1b0e2fb83dc14e9abe1f
David Jantzen <david.jantzen@canvasmedical.com>
2026-01-02 15:43:04 -0800

    example plugin to set staff biography and specialty list

---
+++ staff-plus/staff_plus/models/biography.py
@@ -0,0 +1,20 @@
+from django.db.models import DO_NOTHING, Index, OneToOneField, TextField
+
+from canvas_sdk.v1.data.base import Model
+
+
+class Biography(Model):
+    """An augmented staff profile with biography and stuff"""
+
+    class Meta:
+        db_table = "biography"
+        app_label = "staff_plus"
+
+        indexes = [
+            Index(fields=["staff_id"]),
+        ]
+
+    staff = OneToOneField(
+        "StaffProxy", to_field="dbid", on_delete=DO_NOTHING, null=False, related_name="biography"
+    )
+    biography = TextField()
