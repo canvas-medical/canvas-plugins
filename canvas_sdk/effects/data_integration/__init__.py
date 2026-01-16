afaf2472a05feafcb35e22fa9e66d6f0c544f26f
imran-vicert <imran.agic@vicert.com>
2026-01-16 08:04:57 +0100

    KOALA-3979: Refactor LinkDocumentToPatient effect

--- canvas_sdk/effects/data_integration/__init__.py
+++ canvas_sdk/effects/data_integration/__init__.py
@@ -1,8 +1,5 @@
 from .junk_document import JunkDocument as JunkDocument
 from .junk_document import JunkDocumentConfidenceScores as JunkDocumentConfidenceScores
-from .link_document_to_patient import (
-    LinkDocumentConfidenceScores as LinkDocumentConfidenceScores,
-)
 from .link_document_to_patient import LinkDocumentToPatient as LinkDocumentToPatient
 from .remove_document_from_patient import (
     RemoveDocumentConfidenceScores as RemoveDocumentConfidenceScores,
@@ -12,7 +9,6 @@ from .remove_document_from_patient import RemoveDocumentFromPatient as RemoveDoc
 __all__ = __exports__ = (
     "JunkDocument",
     "JunkDocumentConfidenceScores",
-    "LinkDocumentConfidenceScores",
     "LinkDocumentToPatient",
     "RemoveDocumentConfidenceScores",
     "RemoveDocumentFromPatient",
