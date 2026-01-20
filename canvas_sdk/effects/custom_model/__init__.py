c8f5d3a8d4090dd0b9a9c2f2c30dcd0f3526dd6b
David Jantzen <david.jantzen@canvasmedical.com>
2026-01-20 11:04:36 -0800

    associate models to plugin app after loading

---
+++ canvas_sdk/effects/custom_model/__init__.py
@@ -0,0 +1,11 @@
+from canvas_sdk.effects.custom_model.create import BulkCreate, Create, GetOrCreate
+from canvas_sdk.effects.custom_model.delete import Delete
+from canvas_sdk.effects.custom_model.update import Update
+
+__all__ = __exports__ = (
+    "Create",
+    "BulkCreate",
+    "GetOrCreate",
+    "Update",
+    "Delete",
+)
