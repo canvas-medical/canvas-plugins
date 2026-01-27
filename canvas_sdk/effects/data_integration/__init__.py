c52544ffde58c49f612545efdc11faa2df2d8149
admir-vicert <admir.ihtijarevic@vicert.com>
2026-01-27 16:04:58 +0100

    feat: [KOALA-3944]: Created plugin that will test all effects (#1395)
    Co-authored-by: nikola-milev-vicert <nikola.milev@vicert.com>
    Co-authored-by: imran-vicert <imran.agic@vicert.com>
    Co-authored-by: irfan-vicert <irfan.cajo@vicert.com>

--- canvas_sdk/effects/data_integration/__init__.py
+++ canvas_sdk/effects/data_integration/__init__.py
@@ -1,29 +1,21 @@
 from .assign_document_reviewer import AssignDocumentReviewer as AssignDocumentReviewer
-from .assign_document_reviewer import (
-    AssignDocumentReviewerConfidenceScores as AssignDocumentReviewerConfidenceScores,
-)
 from .assign_document_reviewer import Priority as Priority
 from .assign_document_reviewer import ReviewMode as ReviewMode
 from .junk_document import JunkDocument as JunkDocument
-from .junk_document import JunkDocumentConfidenceScores as JunkDocumentConfidenceScores
-from .link_document_to_patient import (
-    LinkDocumentConfidenceScores as LinkDocumentConfidenceScores,
-)
 from .link_document_to_patient import LinkDocumentToPatient as LinkDocumentToPatient
-from .remove_document_from_patient import (
-    RemoveDocumentConfidenceScores as RemoveDocumentConfidenceScores,
-)
+from .prefill_document_fields import PrefillDocumentFieldData as PrefillDocumentFieldData
+from .prefill_document_fields import PrefillDocumentFields as PrefillDocumentFields
+from .prefill_document_fields import TemplateFields as TemplateFields
 from .remove_document_from_patient import RemoveDocumentFromPatient as RemoveDocumentFromPatient
 
 __all__ = __exports__ = (
     "AssignDocumentReviewer",
-    "AssignDocumentReviewerConfidenceScores",
+    "PrefillDocumentFieldData",
     "JunkDocument",
-    "JunkDocumentConfidenceScores",
-    "LinkDocumentConfidenceScores",
     "LinkDocumentToPatient",
+    "PrefillDocumentFields",
     "Priority",
-    "RemoveDocumentConfidenceScores",
     "RemoveDocumentFromPatient",
     "ReviewMode",
+    "TemplateFields",
 )
