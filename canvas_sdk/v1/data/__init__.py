2e3e0e3eeec10d0c304878bdf1693d129477735f
Beau Gunderson <beau@beaugunderson.com>
2026-02-02 12:58:38 -0800

    add DocumentReference, SnapshotImage, PatientIdentificationCard, and related models

--- canvas_sdk/v1/data/__init__.py
+++ canvas_sdk/v1/data/__init__.py
@@ -33,6 +33,12 @@ from .coverage import Coverage, EligibilitySummary, Transactor, TransactorAddres
 from .detected_issue import DetectedIssue, DetectedIssueEvidence
 from .device import Device
 from .discount import Discount
+from .document_reference import (
+    DocumentReference,
+    DocumentReferenceCategory,
+    DocumentReferenceCoding,
+    DocumentReferenceStatus,
+)
 from .encounter import Encounter
 from .external_event import ExternalEvent, ExternalVisit
 from .facility import Facility
@@ -106,6 +112,7 @@ from .patient import (
     PatientContactPoint,
     PatientExternalIdentifier,
     PatientFacilityAddress,
+    PatientIdentificationCard,
     PatientMetadata,
     PatientSetting,
 )
@@ -144,6 +151,7 @@ from .questionnaire import (
 from .reason_for_visit import ReasonForVisitSettingCoding
 from .referral import Referral, ReferralReport, ReferralReview
 from .service_provider import ServiceProvider
+from .snapshot import Snapshot, SnapshotImage
 from .specialty_report_template import (
     SpecialtyReportTemplate,
     SpecialtyReportTemplateField,
@@ -202,6 +210,10 @@ __all__ = __exports__ = (
     "DetectedIssueEvidence",
     "Device",
     "Discount",
+    "DocumentReference",
+    "DocumentReferenceCategory",
+    "DocumentReferenceCoding",
+    "DocumentReferenceStatus",
     "EligibilitySummary",
     "Encounter",
     "Event",
@@ -278,6 +290,7 @@ __all__ = __exports__ = (
     "PatientContactPoint",
     "PatientExternalIdentifier",
     "PatientFacilityAddress",
+    "PatientIdentificationCard",
     "PatientPosting",
     "PatientSetting",
     "PatientMetadata",
@@ -305,6 +318,8 @@ __all__ = __exports__ = (
     "SpecialtyReportTemplate",
     "SpecialtyReportTemplateField",
     "SpecialtyReportTemplateFieldOption",
+    "Snapshot",
+    "SnapshotImage",
     "Staff",
     "StaffAddress",
     "StaffLicense",
