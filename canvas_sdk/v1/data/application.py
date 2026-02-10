a4566a0b9750e601e1841f8fea551ee6a8fc2ddf
Miguel Quintas <miguel.quintas@canvasmedical.com>
2026-02-10 12:21:27 +0000

    fix: update description

--- canvas_sdk/v1/data/application.py
+++ canvas_sdk/v1/data/application.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Application(models.Model):
-    """Application."""
+    """Plugin Application."""
 
     class Meta:
         db_table = "canvas_sdk_data_plugin_io_application_001"
-        managed = False
 
     identifier = models.CharField(max_length=512, primary_key=True)
     name = models.TextField(null=False, max_length=32)
