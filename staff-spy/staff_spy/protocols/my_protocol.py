0d7f201f1e888c5ac17be63edab66c8352aa94d6
David Jantzen <david.jantzen@canvasmedical.com>
2026-02-18 09:40:45 -0800

    update to showcase namespace sharing

--- staff-spy/staff_spy/protocols/my_protocol.py
+++ staff-spy/staff_spy/protocols/my_protocol.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from hmac import compare_digest
 
+from staff_spy.models.biography import Biography
+from staff_spy.models.language import Language
 from staff_spy.models.proxy import StaffProxy
-from staff_spy.models.specialty import StaffSpecialty
+from staff_spy.models.specialty import Specialty, StaffSpecialty
 
 from canvas_sdk.effects import Effect
 from canvas_sdk.effects.simple_api import JSONResponse, Response
@@ -68,3 +70,60 @@ class MyAPI(SimpleAPI):
             headers={"Authorization": "f2464a67e6fa9839579189a8c1c781e9"},
         )
         return [JSONResponse(response.json())]
+
+    @api.post("/v2/<staff_id>")
+    def post_profile_v2(self):
+        staff_id = self.request.path_params["staff_id"]
+        json_body = self.request.json()
+        staff = StaffProxy.objects.filter(id=staff_id).get()
+        specialty_names = json_body.get("specialties")
+        language_names = json_body.get("languages")
+        practicing_since = json_body.get("practicing_since")
+        accepting_patients = json_body.get("accepting_patients")
+
+        staff.set_attribute("accepting_patients", accepting_patients)
+
+        for l_name in language_names:
+            Language.objects.get_or_create(name=l_name, staff=staff)
+
+        specialties = []
+        for name in specialty_names:
+            specialty, created = Specialty.objects.get_or_create(name=name)
+            specialties.append(specialty)
+
+        # Clear existing associations and create new ones
+        StaffSpecialty.objects.filter(staff=staff).delete()
+
+        staff_specialties = [
+            StaffSpecialty(staff=staff, specialty=specialty) for specialty in specialties
+        ]
+        StaffSpecialty.objects.bulk_create(objs=staff_specialties)
+
+        biography_str = json_body.get("biography")
+        if staff.biography is None:
+            Biography.objects.create(
+                staff=staff, biography=biography_str, practicing_since=practicing_since
+            )
+        else:
+            staff.biography.biography = biography_str
+            staff.biography.languages = "English"
+            staff.biography.practicing_since = practicing_since
+            staff.biography.save()
+
+        staff.refresh_from_db()
+        staff.biography.refresh_from_db()
+        specialties = [s.specialty.name for s in staff_specialties]
+        languages = [l.name for l in staff.languages.all()]
+        profile = {
+            "first_name": staff.first_name,
+            "last_name": staff.last_name,
+            "biography": staff.biography.biography,
+            "specialties": specialties,
+            "languages": languages,
+            "years_of_experience": datetime.today().year - staff.biography.practicing_since
+            if staff.biography.practicing_since
+            else 0,
+            "accepting_patients": staff.get_attribute("accepting_patients"),
+        }
+
+        return [JSONResponse(profile)]
