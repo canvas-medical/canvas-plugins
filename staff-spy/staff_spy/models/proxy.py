32a9cd04c262503e5f686f936fe9a5faaf77bb3e
David Jantzen <david.jantzen@canvasmedical.com>
2026-02-18 14:35:07 -0800

    remove demo plugins from Git

--- staff-spy/staff_spy/models/proxy.py
+++
@@ -1,8 +0,0 @@
-from canvas_sdk.v1.data import CustomAttributeAwareManager, CustomAttributeMixin, Staff
-
-
-class StaffProxy(Staff, CustomAttributeMixin):
-    class Meta:
-        proxy = True
-
-    objects = CustomAttributeAwareManager()
