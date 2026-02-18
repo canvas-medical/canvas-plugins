f7b198824e4e3584cdce21283dd657a89aab7f0c
David Jantzen <david.jantzen@canvasmedical.com>
2026-02-18 14:35:07 -0800

    remove demo plugins from Git

--- staff-spy/staff_spy/models/language.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import DO_NOTHING, ForeignKey, TextField
-from staff_spy.models.proxy import StaffProxy
-
-from canvas_sdk.v1.data.base import CustomModel
-
-
-class Language(CustomModel):
-    staff = ForeignKey(StaffProxy, on_delete=DO_NOTHING, to_field="dbid", related_name="languages")
-    name = TextField()
