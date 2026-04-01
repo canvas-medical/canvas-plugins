47945f47a94ca49c85f27692d7e731c87c3d41ed
Andrew Duane <andrew.duane@canvasmedical.com>
2026-04-01 09:36:03 -0700

    Remove duplicate entry in CANVAS_TOP_LEVEL_MODULES

--- plugin_runner/generate_allowed_imports.py
+++ plugin_runner/generate_allowed_imports.py
@@ -12,7 +12,6 @@ CANVAS_TOP_LEVEL_MODULES = (
     "canvas_sdk.effects",
     "canvas_sdk.events",
     "canvas_sdk.handlers",
-    "canvas_sdk.clients",
     "canvas_sdk.protocols",
     "canvas_sdk.questionnaires",
     "canvas_sdk.templates",
