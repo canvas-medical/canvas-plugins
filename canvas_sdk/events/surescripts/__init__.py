bd2e860e5d26ddc0ef0c504556b016ad39aca796
Nuno Silva <nuno.silva@canvasmedical.com>
2026-06-26 10:21:53 +0100

    feat: add eligibility checks to the sdk (#1688)

---
+++ canvas_sdk/events/surescripts/__init__.py
@@ -0,0 +1,15 @@
+from .surescripts_messages import (
+    BenefitCoverage,
+    EligibilityPlan,
+    SurescriptsBenefitsResponse,
+    SurescriptsEligibilityResponse,
+    TherapeuticAlternative,
+)
+
+__all__ = __exports__ = (
+    "BenefitCoverage",
+    "EligibilityPlan",
+    "SurescriptsBenefitsResponse",
+    "SurescriptsEligibilityResponse",
+    "TherapeuticAlternative",
+)
