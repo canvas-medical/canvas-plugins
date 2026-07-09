84a108da8ce3cce515cf355474e02d8c84cc7348
Miguel Quintas <miguel.quintas@canvasmedical.com>
2026-07-09 10:11:23 +0100

    fix: labtest

--- canvas_sdk/effects/lab_report/__init__.py
+++ canvas_sdk/effects/lab_report/__init__.py
@@ -1,8 +1,9 @@
-from canvas_sdk.effects.lab_report.attach_results import LabValue, ObservationStatus
+from canvas_sdk.effects.lab_report.attach_results import LabTest, LabValue, ObservationStatus
 from canvas_sdk.effects.lab_report.base import LabReport
 
 __all__ = __exports__ = (
     "LabReport",
+    "LabTest",
     "LabValue",
     "ObservationStatus",
 )
