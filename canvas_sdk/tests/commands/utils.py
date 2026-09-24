cade9ed3db07bd5ec4b24e9cf0c895653e958487
Filipe Sousa <filipe.sousa@canvasmedical.com>
2026-09-24 11:23:09 +0100

    feat: add a "Remove Past Medical History" SDK command (#1886)

--- canvas_sdk/tests/commands/utils.py
+++ canvas_sdk/tests/commands/utils.py
@@ -22,6 +22,7 @@ TEST_PLUGINS_DIR = Path(__file__).parent
 # SOME COMMANDS MIGHT NOT BE IN PRODUCTION, SO WE NEED TO SKIP INTEGTEST FOR THEM
 COMMANDS_TO_SKIP = [
     "AddConditionCommand",
+    "RemovePastMedicalHistoryCommand",
     "ImmunizeCommand",
     "LabReviewCommand",
     "ReferralReviewCommand",
